eb131984bfac9dd676f3aa4af6a25782ef44f93f
Андреев Дмитрий <adima@tiberis.mcn.ru>
2013-09-16 21:34:45 +0400

    Класс обработкичк

---
+++ classes/CyberplatRequest.php
@@ -0,0 +1,159 @@
+<?php
+
+
+class CyberPlatRequest
+{
+    // request fields
+    private $data = array();
+
+    public function proccessRequest()
+    {
+        try{
+            $this->do_action(get_param_raw("action"));
+        }catch(CyberplatError $e)
+        {
+            $this->echoError($e);
+            exit();
+        }catch(CyberplatOK $e)
+        {
+            $this->echoOK($e);
+            exit();
+        }catch(Exception $e)
+        {
+            // error code 10 && mail error
+            $this->log(array("unexcepted errror", $e));
+        }
+    }
+
+    private function do_action($action)
+    {
+        if($action)
+        {
+            $this->data = $this->_load();
+            $this->log($this->data);
+        }
+
+        switch($action)
+        {
+            case 'check': $this->check(); break;
+            case 'payment': $this->payment(); break;
+            case 'cancel': $this->cancel(); break;
+            case 'status': $this->status(); break;
+            default: 
+                throw new Answer_ERR_ACTION();
+        }
+    }
+
+    private function check()
+    {
+        if(!$this->data["type"] || $this->data["type"] != 1)
+            throw new Answer_ERR_TYPE();
+
+        if(!$this->data["amount"] || $this->data["amount"] <= 0)
+            throw new Answer_ERR_BAD_AMOUNT();
+        
+        $c = ClientCard::find_by_id($this->data["number"]);
+        if($c)
+        {
+            throw new Answer_OK("Абонент найден");
+        }else
+            throw new Answer_ERR_CLIENT_NOT_FOUND();;
+    }
+
+    private function payment()
+    {
+        throw new CyberplatError("Внутренная ошибка");
+    }
+
+    private function cancel()
+    {
+        throw new CyberplatError("Внутренная ошибка");
+    }
+
+    private function status()
+    {
+        throw new CyberplatError("Внутренная ошибка");
+    }
+
+    private function _load()
+    {
+        $data = array();
+        foreach(array("number", "amount", "type", "sign", "receipt", "date", "mes", "additional") as $f)
+        {
+            $data[$f] = get_param_protected($f);
+        }
+
+        return $data;
+
+    }
+
+    private function log($d)
+    {
+        $pFile = fopen(LOG_DIR."cyberplat.log", "a+");
+        fwrite($pFile, "\n".date("r").": ".var_export($d, true));
+        fclose($pFile);
+    }
+
+    private function echoError($e)
+    {
+        echo "<?xml version=\"1.0\" encoding=\"koi8-r\"?>\n".
+            "<response>\n".
+            "   <code>".$e->getCode()."</code>\n".
+            "   <message>".iconv("utf-8", "koi8-r", $e->getMessage())."</message>\n".
+            "</response>\n";
+    }
+
+    private function echoOK($e)
+    {
+        echo "<?xml version=\"1.0\" encoding=\"koi8-r\"?>\n".
+            "<response>\n".
+            "   <code>0</code>\n".
+            "   <message>".iconv("utf-8", "koi8-r", $e->getMessage())."</message>\n".
+            "</response>\n";
+
+    }
+}
+
+
+class CyberplatError extends Exception
+{
+    public $code = 10;
+    public $message = "";
+}
+
+abstract class CyberplatOK extends Exception
+{
+    public $code = 0;
+    public $message = "";
+}
+
+class Answer_ERR_TYPE extends CyberplatError
+{
+    public $code = -2;
+    public $message = "Неверное значение платежа";
+}
+
+class Answer_ERR_ACTION extends CyberplatError
+{
+    public $code = 1;
+    public $message = "Неизвестный тип запроса";
+}
+
+class Answer_ERR_CLIENT_NOT_FOUND extends CyberplatError
+{
+    public $code = 2;
+    public $message = "Лицевой счет не найден";
+}
+
+class Answer_ERR_BAD_AMOUNT extends CyberplatError
+{
+    public $code = 3;
+    public $message = "Неверная сумма платежа";
+}
+
+class Answer_OK extends CyberplatOK
+{
+    //
+}
+
+
