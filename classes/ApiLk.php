d28c38bfdbb5124e6679039d84479cae5f0b5b9c
Andreev Dmitry <adima@mcn.ru>
2014-04-22 17:36:27 +0400

    Корректировка уловия выдачи свободных номеров в московском регионе

--- classes/ApiLk.php
+++ classes/ApiLk.php
@@ -738,7 +738,7 @@ class ApiLk
                     voip_numbers v 
                 )a 
             LEFT JOIN clients c ON (c.id = a.client_id) 
-            WHERE if(a.region = 99, number like '74951059%' or a.beauty_level in (1,2), true)
+            WHERE if(a.region = 99, if(number like '7495%', number like '74951059%' or beauty_level in (1,2),true), true)
             HAVING status IN ('free')";
         
         foreach(NewBill::find_by_sql($q/*"
