e770b803d74b08afa34e16a0263204c48768a1c0
Andreev Dmitry <adima@mcn.ru>
2014-05-12 15:09:56 +0400

    Мелкие доработки, hot fix'ы

--- utils/new_vpbx_tarif_convertor.php
+++ utils/new_vpbx_tarif_convertor.php
@@ -11,7 +11,7 @@ print "Starting: " . date('Y-m-d H:i') . "\n";
 $vpbx = $db->AllRecords('select * from usage_virtpbx');
 foreach ($vpbx as $r) {
     $db->Query('insert into log_tarif (service,id_service,id_tarif,id_user,ts,comment,date_activation,actual_from, actual_to) VALUES '.
-            '("usage_virtpbx",'.$r['id'].','.intval($r['tarif_id']).',48,NOW(),"","'.addslashes($r['actual_from']).'","'.addslashes($r['actual_from']).'","'.addslashes($r['actual_to']).'")');
+            '("usage_virtpbx",'.$r['id'].','.intval($r['tarif_id']).',0,NOW(),"","'.addslashes($r['actual_from']).'","'.addslashes($r['actual_from']).'","'.addslashes($r['actual_to']).'")');
     
 }
 
