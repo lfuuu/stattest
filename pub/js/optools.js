f502b0d1ade6908d57b3ad256df0ec7ff0614666
Andreev Dmitry <adima@mcn.ru>
2014-05-29 18:40:27 +0400

    Заведены номера региона "Казань" (93)

--- pub/js/optools.js
+++ pub/js/optools.js
@@ -834,6 +834,9 @@ $(document).ready(function() {
 			} else if(region_id == '94') {
 				get_free_e164
 					.append("<option value='7383312'>7(383) 312</option>");
+			} else if(region_id == '93') {
+				get_free_e164
+					.append("<option value='7843207'>7(843) 207</option>");
 			} else if(region_id == '87') {
 				get_free_e164
 					.append("<option value='7863309'>7(863) 309</option>");
