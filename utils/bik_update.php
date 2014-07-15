af40f204957c5f1629ec018061a2ab4223b9a98b
Sergei Kugubaev <kubik073@yandex.ru>
2014-07-15 12:32:29 +0400

    Fix BikUpdater

--- utils/bik_update.php
+++ utils/bik_update.php
@@ -2,43 +2,40 @@
 	define("PATH_TO_ROOT",'../');
 	include PATH_TO_ROOT."conf.php";
 	include PATH_TO_ROOT."include/MyDBG.php";
-	//--------------------------------------------------------------------------------------------------
-	if (date('j') != 5 && !empty($_SERVER['argv'])) 
-	{
-		echo 'Wrong day to update Bik';
-		exit;
-	}
-	// проверка параметров от сервера
-	if ($_SERVER['argv'][1] == '-h' || $_SERVER['argv'][1] == '-help') {
-		echo "\n" . Encoding::toUtf8("Первый параметр true - загрузка нового файла, false - использовать старый файл (по умолчанию false)\n\n");
-		echo Encoding::toUtf8("Второй параметр true - внести изменения в БД, false - без изменений в БД(по умолчанию false)\n");
-		echo Encoding::toUtf8("Третий параметр true - вывод на экран, false - отправка email (по умолчанию false)\n\n");
-		exit;
-	}
-	$loadFile = (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'true') ? true : false;
-	$isUpdate = (isset($_SERVER['argv'][2]) && $_SERVER['argv'][2] == 'true') ? true : false;
-	$toScreen = (isset($_SERVER['argv'][3]) && $_SERVER['argv'][3] == 'true') ? true : false;
 	
-	// проверка парамтров из запроса
-	if (!$loadFile && isset($_GET['load_file']) && $_GET['load_file'] == 1) 
-	{
-		$loadFile = true;
-	}
-	if (!$isUpdate && isset($_GET['make_update']) && $_GET['make_update'] == 1) 
-	{
-		$isUpdate = true;
-	}
-	if (!$toScreen && isset($_GET['to_screen']) && $_GET['to_screen'] == 1) 
-	{
-		$toScreen = true;
-	}
+	$loadFile = $sendDiff = $toScreen = $isUpdate = false;
 	
+	switch ($_SERVER['argv'][1]) {
+		case 'get':
+			$loadFile = true;
+			break;
+		case 'send': 
+			$sendDiff = true;
+			break;
+		case 'show':
+			$toScreen = true;
+			break;
+		case 'update':
+			$isUpdate = true;
+			$toScreen = true;
+			break;
+		default: 
+			echo "\n" . Encoding::toUtf8("Параметр =  get - загрузка нового файла\n\n");
+			echo Encoding::toUtf8("Параметр = send - отправка email с diff-файлом\n");
+			echo Encoding::toUtf8("Параметр = show - вывод на экран разницы\n");
+			echo Encoding::toUtf8("Параметр = update - вывод на экран разницы и внести изменения в БД \n\n");
+			exit;
+	}
+
 	if ($loadFile)
 	{
 		$file_path = LoadBikFile::getBikFile('http://www.cbr.ru/mcirabis/PluginInterface/GetBicCatalog.aspx');
 		if ($file_path === false)
 		{
-			echo 'incorrect day or no file';
+			echo "\n" . Encoding::toUtf8("Файл не был обновлен\n\n");
+			exit;
+		} else {
+			echo "\n" . Encoding::toUtf8("Файл был обновлен\n\n");
 			exit;
 		}
 	} else {
@@ -47,13 +44,28 @@
 	
 	$BikUpdaterDBF = new BikUpdaterDBF($file_path);
 
-	if (!$BikUpdaterDBF->readMyDBF()) {die("Bad file format ($file_path)\n");}
+	if (!$BikUpdaterDBF->readMyDBF()) 
+	{
+		die("Bad file format ($file_path)\n");
+	}
 
 	$BikUpdaterDBF->run();
-
-	$BikUpdaterDBF->Logs($toScreen);
-
+	
+	if ($sendDiff) 
+	{
+		if ($BikUpdaterDBF->sendLogs()) 
+		{
+			echo "\n" . Encoding::toUtf8("Файл был отправлен\n\n");
+		}
+	}
+	
+	if ($toScreen) 
+	{
+		$BikUpdaterDBF->showLog();
+	}
+	
 	if ($isUpdate) 
+	{
 		$BikUpdaterDBF->updateBik();
-
+	}
 ?>
