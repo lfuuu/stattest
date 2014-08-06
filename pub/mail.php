6b6f16e634bedfa8a4fa6c8523183b45101b9ebf
Andreev Dmitry <adima@mcn.ru>
2014-08-06 18:22:51 +0400

    Из ЛК можно посмотреть документ УПД.

--- pub/mail.php
+++ pub/mail.php
@@ -6,7 +6,7 @@
 	$o = MailJob::GetObjectP();
 	$db->Query('update mail_object set view_count=view_count+1, view_ts = IF(view_ts=0,NOW(),view_ts) where object_id='.$o['object_id']);
 
-	if (in_array($o["object_type"], array("bill", "assignment", "order", "notice", "invoice","akt", "lading", "new_director_info"))) {
+	if (in_array($o["object_type"], array("bill", "assignment", "order", "notice", "invoice","akt", "lading", "new_director_info", "upd"))) {
         if($o["object_type"] == "assignment" && $o["source"] == 2)
             $o["source"] = 4;
 		$R = array();
