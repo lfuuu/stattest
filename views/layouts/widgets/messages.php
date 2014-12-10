42d2ce39af0db7ec216f1698b2de858f1bdf2f23
Иванов Евгений <e45456@yandex.com>
2014-12-10 19:17:19 +0300

    Update messages.php

--- views/layouts/widgets/messages.php
+++ views/layouts/widgets/messages.php
@@ -1,13 +1,10 @@
-<?php
-use \yii\helpers\Html;
-?>
 <?php if (Yii::$app->session->hasFlash('error')): ?>
-    <?php foreach(Yii::$app->session->getFlash('error') as $message): ?>
+    <?php foreach(Yii::$app->session->getFlash('error', [], true) as $message): ?>
         <div style="color:#ff0000; font-weight: bold"><?= $message ?></div>
     <?php endforeach; ?>
 <?php endif; ?>
 <?php if (Yii::$app->session->hasFlash('success')): ?>
-    <?php foreach(Yii::$app->session->getFlash('success') as $message): ?>
+    <?php foreach(Yii::$app->session->getFlash('success', [], true) as $message): ?>
         <div style="color:#008000; font-weight: bold"><?= $message ?></div>
     <?php endforeach; ?>
 <?php endif; ?>
