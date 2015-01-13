d57465c50a398997fee5fef17ddb3ce4b95b2e58
Vladimir Popov <vladimirpopov1980@gmail.com>
2015-01-13 15:04:15 +0300

    обновленный вью

--- views/clients/index.php
+++ views/clients/index.php
@@ -1,77 +1,13 @@
 <?php
 
 use yii\grid\GridView;
-use app\classes\yii\McnSqlDataProvider;
-use app\classes\yii\GlyphDataColumn;
-use app\classes\yii\HrefDataColumn;
-
-use yii\helpers\Url;
-
-use app\classes\yii\BaseController;
-use app\classes\Encoding;
-
-require(__DIR__ . '/../../controllers/config/grid.php');
+use yii;
 
 $this->registerCssFile('/bootstrap/css/bootstrap.min.css');
 $this->registerCssFile('/css/grid/grid.css');   
 
-$bp = Yii::$app->request->get('bp', 'telecom.accounting');
-$bp = explode('.', $bp);
-$filtered_rows = $menumap[$bp[0]][$bp[1]];
 
-foreach ($filtered_rows as $filtered_row)
-{
-    if(isset($rows[$filtered_row]))
-    {    
-        $temp_rows[$filtered_row] = $rows[$filtered_row];
-    }
-}
-
-$rows = $temp_rows;
-
-$filtername = Yii::$app->request->get('status_filter');
-
-if(!isset($rows[$filtername])) 
-{
-   $filtername = reset($filtered_rows);
-}
-
-$row = $rows[$filtername];
-
-if(!isset($row)) return;
-
-$dataProvider = new McnSqlDataProvider([
-    'sql' => $row['sql'],
-    //'totalcount' => 1000, 
-    'sort' => [
-        'attributes' => $row['sortable'],
-        'defaultOrder' => $row['order'],
-    ],
-    'pagination' => [
-        'pageSize' => $row['countperpage']
-    ],
-]);
-
-
-$providerfields = array_keys(reset($dataProvider->getModels()));
-
-foreach( $providerfields as $field)
-{  
-    
-    foreach( $row['columns'][$field] as $key => $value )
-    {
-        $column[$key] = $value;
-    }
-
-    if(isset($column)&&!$column['hide']) 
-    { 
-        //нужно повторить названия полей провайдера для грида
-        $column['attribute'] = $field;
-        $columns[] = $column; 
-    }
-    
-    unset( $label, $class, $column );
-}
+//var_dump($row); exit();
 
 $urlmanager = Yii::$app->getUrlManager();
 $url_params[0] = Yii::$app->controller->getRoute();
@@ -81,11 +17,11 @@ $url_params[0] = Yii::$app->controller->getRoute();
 
     <ul class="nav nav-tabs">
      <?php foreach ($rows as $key => $value) { 
-         $url_params['status_filter'] = $key;
-         $url_params['bp'] = Yii::$app->request->get('bp');
+         $url_params['grid'] = $value['id'];
+         $url_params['bp'] = $value['client_bp_id'];
          $link = $urlmanager->createUrl($url_params);
      ?>
-        <li role="presentation" <?php if( $filtername == $key ) { ?>class="active"<?php } ?>><a href="<?= $link ?>"><?= $value['header'] ?></a></li>
+        <li role="presentation" <?php if( $value['id'] == $row['id'] ) { ?>class="active"<?php } ?>><a href="<?= $link ?>"><?= $value['name'] ?></a></li>
      <?php } ?>
     </ul>
 
@@ -93,7 +29,9 @@ $url_params[0] = Yii::$app->controller->getRoute();
 
 <?php
 
-echo GridView::widget([
-   'dataProvider' => $dataProvider,
-   'columns' => $columns,
-]);
+if($dataProvider->TotalCount > 0) {
+    echo GridView::widget([
+       'dataProvider' => $dataProvider,
+       'columns' => $columns,
+    ]);
+}
