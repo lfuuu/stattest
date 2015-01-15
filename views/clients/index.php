efe9b0dbe2f630e73301fcd800f4f307c2d63d76
Vladimir Popov <vladimirpopov1980@gmail.com>
2015-01-15 16:02:34 +0300

    корректировка названий таблиц и классов

--- views/clients/index.php
+++ views/clients/index.php
@@ -11,10 +11,10 @@ $url_params[0] = Yii::$app->controller->getRoute();
     <ul class="nav nav-tabs">
      <?php foreach ($rows as $key => $value) { 
          $url_params['grid'] = $value['id'];
-         $url_params['bp'] = $value['client_bp_id'];
+         $url_params['bp'] = $value['grid_business_process_id'];
          $link = $urlmanager->createUrl($url_params);
      ?>
-        <li role="presentation" <?php if( $value['id'] == $row['id'] ) { ?>class="active"<?php } ?>><a href="<?= $link ?>"><?= $value['name'] ?></a></li>
+        <li role="presentation" <?php if ( $value['id'] == $row['id'] ) { ?>class="active"<?php } ?>><a href="<?= $link ?>"><?= $value['name'] ?></a></li>
      <?php } ?>
     </ul>
 
@@ -22,7 +22,7 @@ $url_params[0] = Yii::$app->controller->getRoute();
 
 <?php
 
-if($dataProvider->TotalCount > 0) {
+if($dataProvider->totalCount > 0) {
     echo GridView::widget([
        'dataProvider' => $dataProvider,
        'columns' => $columns,
