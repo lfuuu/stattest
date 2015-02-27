dc34edaddf2dd81e48fc50c52c096ca749ce8431
Vladimir Popov <vladimirpopov1980@gmail.com>
2015-02-27 17:09:30 +0300

    Обновление статусов

--- migrations/migrations/m150227_132847_update_statuses.php
+++ migrations/migrations/m150227_132847_update_statuses.php
@@ -1,19 +1,19 @@
 <?php
 
-class m260218_132846_update_statuses extends \app\classes\Migration
+class m150227_132847_update_statuses extends \app\classes\Migration
 {
     public function up()
     {
         
         //добавление и замена строчек таблицы grid_settings
-        $this->executeSqlFile('grid_settings_dump.sql');
+        $this->executeSqlFile('grid_settings_dump.sql'); 
         
 
     }
 
     public function down()
     {
-        echo "m260218_132846_update_statuses cannot be reverted.\n";
+        echo "m150227_132847_update_statuses cannot be reverted.\n";
 
         return false;
     }
