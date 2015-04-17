4c929f69930c0aac895883520a8fab394bb3c54a
Иванов Евгений <e45456@yandex.com>
2015-04-17 14:18:57 +0300

    Update service.php

--- web/operator/1c/service.php
+++ web/operator/1c/service.php
@@ -2,6 +2,7 @@
 define("PATH_TO_ROOT",'../../../stat/');
 require_once "../../../stat/conf_yii.php";
 
+use app\models\User;
 Yii::$app->user->setIdentity(User::findOne(User::SYSTEM_USER_ID));
 
 Header('Content-type: text/xml; charset="UTF-8"');
