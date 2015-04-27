17dbb98bfd5e26e07fb2f86bbe4eef31f86477fb
Vladimir Popov <vladimirpopov1980@gmail.com>
2015-04-27 11:15:31 +0300

    Коррекция НД списков фильтров

--- classes/grid/filters/simplefilterfield/AccountManagerSimpleFilterField.php
+++ classes/grid/filters/simplefilterfield/AccountManagerSimpleFilterField.php
@@ -8,12 +8,12 @@ class AccountManagerSimpleFilterField extends SimpleFilterField
     public $sql_field_name = 'account_manager';
     
     protected function initDataset()
-    {
+    {     
         $this->filterValuesQuery
-            ->select('user as id, name')
-            ->from('user_users')
-            ->where('usergroup = "account_managers"')
-            ->orderBy('name');
+            ->select('account_manager AS id, account_manager AS name')
+            ->from('clients')
+            ->groupBy('account_manager')
+            ->orderBy('account_manager');
     }
     
 }
--- classes/grid/filters/simplefilterfield/ManagerSimpleFilterField.php
+++ classes/grid/filters/simplefilterfield/ManagerSimpleFilterField.php
@@ -8,12 +8,12 @@ class ManagerSimpleFilterField extends SimpleFilterField
     public $sql_field_name = 'manager';
     
     protected function initDataset()
-    {
+    {     
         $this->filterValuesQuery
-            ->select('user as id, name')
-            ->from('user_users')
-            ->where('usergroup = "manager"')
-            ->orderBy('name');
+            ->select('manager AS id, manager AS name')
+            ->from('clients')
+            ->groupBy('manager')
+            ->orderBy('manager');
     }
     
     
