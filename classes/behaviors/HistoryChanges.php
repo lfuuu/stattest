e4cb6fa5fc4bacd1c2a6f71c813c2d08792e48b3
Иванов Евгений <e45456@yandex.com>
2015-05-14 16:39:10 +0300

    Update HistoryChanges.php

--- classes/behaviors/HistoryChanges.php
+++ classes/behaviors/HistoryChanges.php
@@ -11,7 +11,7 @@ class HistoryChanges extends Behavior
     {
         return [
             ActiveRecord::EVENT_AFTER_INSERT => 'afterInsert',
-            ActiveRecord::EVENT_AFTER_UPDATE => 'afterUpdate',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'beforeUpdate',
             ActiveRecord::EVENT_AFTER_DELETE => 'afterDelete',
         ];
     }
@@ -21,7 +21,7 @@ class HistoryChanges extends Behavior
         $this->logChanges(\app\models\HistoryChanges::ACTION_INSERT, $this->owner->toArray(), null);
     }
 
-    public function afterUpdate()
+    public function beforeUpdate()
     {
         $this->fillChanges($data, $prevData);
         if (!empty($data)) {
