fef722e945b9c7f23011996a74724dcac30c474b
Evgeniy Ivanov <e45456@yandex.ru>
2015-05-14 19:05:53 +0300

    added currencies HUF, EUR

--- models/Currency.php
+++ models/Currency.php
@@ -7,10 +7,14 @@ class Currency extends ActiveRecord
 {
     const RUB = 'RUB';
     const USD = 'USD';
+    const HUF = 'HUF';
+    const EUR = 'EUR';
 
     private static $symbols = [
         self::RUB => 'руб.',
         self::USD => '$',
+        self::HUF => 'HUF',
+        self::EUR => 'EUR',
     ];
 
     public static function tableName()
