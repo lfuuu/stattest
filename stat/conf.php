5786a79b00dba81e6d56d3a9eeb1250315bc2152
Дмитрий <dga@mcn.ru>
2015-07-01 19:40:26 +0300

    fix_trigger_error2
    Доработана функция trigger_error2 для работы с консольными скриптами

--- stat/conf.php
+++ stat/conf.php
@@ -203,5 +203,10 @@ ActiveRecord\Config::initialize(function($cfg) {
 });
 
 function trigger_error2($string) {
-    Yii::$app->session->addFlash('error', $string);
+    if (Yii::$app instanceof \app\classes\WebApplication)                                                                        │
+    {                                                                                                                            │Did you mean one of these?
+       Yii::$app->session->addFlash('error', $string);                                                                      │        reset
+    } else {                                                                                                                     │        stage
+       echo $string."\n";                                                                                                   │        stash
+    } 
 }
