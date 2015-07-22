d792986e84d4d4516a0e4df6bc6a0475beb7fd2a
Андреев Дмитрий <adima@mcn.ru>
2015-07-22 20:32:32 +0300

    Соглашение о передачи прав "МСМ Телеком"

--- classes/documents/DocumentReportFactory.php
+++ classes/documents/DocumentReportFactory.php
@@ -18,7 +18,9 @@ class DocumentReportFactory extends Singleton
     {
         return [
             BillDocRepRuRUB::className(),
-            BillDocRepHuHUF::className()
+            BillDocRepHuHUF::className(),
+            DocSoglMCMTelekom::className()
+
         ];
     }
 
