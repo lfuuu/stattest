dc3219fd3be3bb7f1047e0692bd11500a2e6196e
Roman Elistratov <romane@mcn.ru>
2015-07-23 10:36:17 +0300

    BIL-323. VoipPackageBiller

---
+++ classes/bill/VoipPackageBiller.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\classes\bill;
+
+use Yii;
+use app\models\TariffVoipPackage;
+
+class VoipPackageBiller extends Biller
+{
+
+    /** @var TariffVoipPackage */
+    private $tariff = false;
+
+    public function getTranslateFilename()
+    {
+        return 'biller-voip';
+    }
+
+    protected function processPeriodical()
+    {
+        $package = BillerPackagePeriodical::create($this)
+            ->setPeriodType(self::PERIOD_MONTH)// Need for localization
+            ->setPrice($this->usage->tariff->periodical_fee)
+            ->setTemplate('voip_package_fee')
+            ->setTemplateData([
+                'tariff' => $this->usage->tariff->name,
+                'service' => $this->usage->usageVoip->E164,
+            ]);
+        $this->addPackage($package);
+    }
+
+    protected function processResource()
+    {
+        $this->addPackage(
+            BillerPackageResource::create($this)
+                ->setPrice($this->usage->tariff->periodical_fee)
+                ->setMinPayment($this->usage->tariff->min_payment)
+                ->setMinPaymentTemplate('voip_package_minpay')
+                ->setPeriodType(self::PERIOD_MONTH)// Need for localization
+                ->setTemplate('voip_package_payment')
+                ->setTemplateData([
+                    'tariff' => $this->usage->tariff->name,
+                    'service' => $this->usage->usageVoip->E164,
+                ])
+        );
+    }
+}
