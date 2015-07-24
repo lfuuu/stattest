6f8df18af9d0eeee365599c445336f94d155a210
sooonic <stepan.okhorzin@gmail.com>
2015-07-24 13:08:31 +0400

    .

--- migrations/migrations/m150723_180421_tax_regime.php
+++ migrations/migrations/m150723_180421_tax_regime.php
@@ -5,10 +5,11 @@ class m150723_180421_tax_regime extends \app\classes\Migration
     public function up()
     {
         $this->execute("
-            ALTER TABLE `client_contragent`
-            	CHANGE COLUMN `tax_regime` `tax_regime` ENUM('0','1') NOT NULL DEFAULT '1' AFTER `fioV`;
-
-            UPDATE `client_contragent` SET `tax_regime` = 1;
+            ALTER TABLE `client_contragent` ADD COLUMN `t` ENUM('0','1') NOT NULL DEFAULT '1' AFTER `tax_regime`;
+            UPDATE `client_contragent` SET `t` = '0';
+            UPDATE `client_contragent` SET `t` = '1' WHERE `tax_regime` != 'simplified';
+            ALTER TABLE `client_contragent` DROP COLUMN `tax_regime`;
+            ALTER TABLE `client_contragent` CHANGE COLUMN `t` `tax_regime` ENUM('0','1') NOT NULL DEFAULT '1' AFTER `fioV`;
         ");
     }
 
