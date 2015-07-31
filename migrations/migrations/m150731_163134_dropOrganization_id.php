272ef75e0230eca7b8c0120c5096ca4518ebdfff
sooonic <stepan.okhorzin@gmail.com>
2015-07-31 21:25:59 +0400

    migration

--- migrations/migrations/m150731_163134_dropOrganization_id.php
+++ migrations/migrations/m150731_163134_dropOrganization_id.php
@@ -7,6 +7,15 @@ class m150731_163134_dropOrganization_id extends \app\classes\Migration
         $this->execute("
             ALTER TABLE `clients`
                 DROP COLUMN `organization_id`;
+
+            UPDATE history_version SET data_json = REPLACE(data_json,
+            SUBSTRING(history_version.`data_json`,
+                LOCATE('\"organization_id\":', history_version.`data_json`),
+                 18 + LOCATE(',', SUBSTRING(history_version.`data_json`, LOCATE('\"organization_id\":', history_version.`data_json`)+18,5))
+              ), ''
+            )
+            WHERE model = 'ClientAccount' AND data_json LIKE '%\"organization_id\":%'
+            ;
         ");
     }
 
