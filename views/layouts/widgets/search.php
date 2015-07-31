ae4131894621c4dffd05adacbed506f693b36642
sooonic <stepan.okhorzin@gmail.com>
2015-07-31 18:55:04 +0400

    http://bugtracker.welltime.ru/jira/browse/BIL-382

--- views/layouts/widgets/search.php
+++ views/layouts/widgets/search.php
@@ -28,37 +28,37 @@ $request = Yii::$app->request->get();
         <div class="col-sm-8">
             <div class="btn-group" id="btn-options">
                 <button type="submit" class="btn btn-link btn-xs">Искать по</button>
-                <button type="submit" class="btn btn-primary btn-xs" style="display: none;" data-search="clients"
+                <button type="submit" class="btn btn-primary btn-xs" data-search="clients"
                         data-placeholder="№ ЛС или Названию">Клиентам
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="contractNo"
+                <button type="submit" class="btn btn-default btn-xs" data-search="contractNo"
                         data-placeholder="Номеру договора">Договору
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="inn"
+                <button type="submit" class="btn btn-default btn-xs" data-search="inn"
                         data-placeholder="ИНН">ИНН
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="voip"
+                <button type="submit" class="btn btn-default btn-xs" data-search="voip"
                         data-placeholder="номеру">Voip
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="email"
+                <button type="submit" class="btn btn-default btn-xs" data-search="email"
                         data-placeholder="email">Email
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="troubles"
+                <button type="submit" class="btn btn-default btn-xs" data-search="troubles"
                         data-placeholder="№ заявки">Заявкам
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="bills"
+                <button type="submit" class="btn btn-default btn-xs" data-search="bills"
                         data-placeholder="№ счёта">Счетам
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="ip"
+                <button type="submit" class="btn btn-default btn-xs" data-search="ip"
                         data-placeholder="IP адресу">IP
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="address"
+                <button type="submit" class="btn btn-default btn-xs" data-search="address"
                         data-placeholder="адресу">Адресу
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="domain"
+                <button type="submit" class="btn btn-default btn-xs" data-search="domain"
                         data-placeholder="домену">Домену
                 </button>
-                <button type="submit" class="btn btn-default btn-xs" style="display: none;" data-search="adsl"
+                <button type="submit" class="btn btn-default btn-xs" data-search="adsl"
                         data-placeholder="ADSL">ADSL
                 </button>
             </div>
@@ -75,7 +75,7 @@ $request = Yii::$app->request->get();
             if ($('#search-type').val()) {
                 $('#btn-options .btn:not(.btn-link)').addClass('btn-default').removeClass('btn-primary');
                 $('.btn[data-search="' + $('#search-type').val() + '"]').removeClass('btn-default').addClass('btn-primary');
-                $('#btn-options .btn-link').click();
+                //$('#btn-options .btn-link').click();
             }
             setInput();
 
@@ -122,11 +122,12 @@ $request = Yii::$app->request->get();
                 });
         });
 
+        /*
         $('#btn-options .btn-link').on('click', function (e) {
             e.preventDefault();
             $(this).parent().children(':not(.btn-link)').toggle();
             $('.layout_main , .layout_left ').css('top', $('#top_search').closest('.row').height()+25);
-        });
+        });*/
 
         $('#btn-options .btn:not(.btn-link)').on('click', function (e) {
             e.preventDefault();
