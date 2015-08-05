c6bd54e8770acdf860d0c07d03798138cb4a597d
sooonic <stepan.okhorzin@gmail.com>
2015-08-05 18:11:56 +0400

    392

--- stat/modules/voipreports/calls_report.php
+++ stat/modules/voipreports/calls_report.php
@@ -12,18 +12,14 @@ class m_voipreports_calls_report
         global $design, $pg_db;
         set_time_limit(0);
 
-        $f_instance_id = (int)get_param_protected('f_instance_id', '0');
         $f_operator_id = (int)get_param_protected('f_operator_id', '0');
         $date_from = get_param_protected('date_from', date('Y-m-d'));
         $date_to = get_param_protected('date_to', date('Y-m-d'));
         $f_country_id = get_param_protected('f_country_id', '0');
         $f_region_id = get_param_protected('f_region_id', '0');
-        $f_dest_group = get_param_protected('f_dest_group', '');
         $f_direction_out = get_param_protected('f_direction_out', 't');
         $f_mob = get_param_protected('f_mob', '0');
         $f_server_id = get_param_protected('f_server_id', '0');
-        $f_prefix_op = get_param_protected('f_prefix_op', '');
-        $f_without_prefix_op = get_param_protected('f_without_prefix_op', '0');
         $limit = 100;
         $offset = get_param_integer('offset', 0);
 
@@ -34,8 +30,8 @@ class m_voipreports_calls_report
 
         if (isset($_GET['makeFile']) || isset($_GET['make'])) {
             $where = " billed_time>0";
-            if ($f_instance_id != '0')
-                $where .= " and server_id = {$f_instance_id} ";
+            if ($f_server_id != '0')
+                $where .= " and server_id = {$f_server_id} ";
             $where .= " and r.connect_time >= '{$date_from}'";
             $where .= " and r.connect_time <= '{$date_to} 23:59:59'";
             $where .= $f_direction_out == 'f' ?  " and r.orig=true " : " and r.orig=false ";
@@ -43,13 +39,6 @@ class m_voipreports_calls_report
             if ($f_operator_id != '0')
                 $where .= " and r.operator_id='{$f_operator_id}' ";
 
-            if ($f_dest_group != '') {
-                if ($f_dest_group == '-1') {
-                    $where .= " and r.destination_id < 0 ";
-                } else {
-                    $where .= " and r.destination_id='{$f_dest_group}' ";
-                }
-            }
             if ($f_country_id != '0')
                 $where .= " and g.country='{$f_country_id}' ";
             if ($f_region_id != '0')
@@ -60,10 +49,6 @@ class m_voipreports_calls_report
                 $where .= " and r.mob=false ";
             if ($f_region_id != '0')
                 $where .= " and g.region='{$f_region_id}' ";
-            if ($f_prefix_op)
-                $where .= " and r.prefix = '{$f_prefix_op}'";
-            if ($f_without_prefix_op > 0)
-                $where .= " and r.prefix is null ";
 
         }
 
@@ -89,6 +74,7 @@ class m_voipreports_calls_report
                 ob_end_flush();
             }
 
+            echo "CONNECT_TIME;SRC_NUMBER;DST_NUMBER;BILLED_TIME\n";
             while(true) {
                 $row = $pg_db->NextRecord(PGSQL_NUM);
                 if (0 === $row) {
@@ -118,10 +104,8 @@ class m_voipreports_calls_report
                               r.billed_time,
                               r.cost,
                               r.operator_id,
-                              r.destination_id,
                               g.name as destination,
-                              r.server_id,
-                              r.prefix
+                              r.server_id
                         from calls_raw.calls_raw r
                         left join voip_destinations d on d.ndef=r.geo_id
                         left join geo.geo g on g.id=d.geo_id
@@ -146,7 +130,7 @@ class m_voipreports_calls_report
         $design->assign('limit', $limit);
         $design->assign('item_from', count($report) ? $offset + 1 : 0);
         $design->assign('item_to', count($report) ? $offset + $limit : 0);
-        $design->assign('f_instance_id', $f_instance_id);
+        $design->assign('f_server_id', $f_server_id);
         $design->assign('f_operator_id', $f_operator_id);
         $design->assign('date_from', $date_from);
         $design->assign('date_to', $date_to);
@@ -154,9 +138,6 @@ class m_voipreports_calls_report
         $design->assign('f_region_id', $f_region_id);
         $design->assign('f_direction_out', $f_direction_out);
         $design->assign('f_mob', $f_mob);
-        $design->assign('f_prefix_op', $f_prefix_op);
-        $design->assign('f_without_prefix_op', $f_without_prefix_op);
-        $design->assign('f_dest_group', $f_dest_group);
         $design->assign('operators', $operators);
         $design->assign('geo_countries', $pg_db->AllRecords("SELECT id, name FROM geo.country ORDER BY name"));
         $design->assign('geo_regions', $pg_db->AllRecords("SELECT id, name FROM geo.region ORDER BY name"));
