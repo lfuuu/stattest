8e468620ef198e1dff644b189d1506d9f10e3513
sooonic <stepan.okhorzin@gmail.com>
2015-08-17 16:10:42 +0400

    519

--- views/client/block/rightmenu.php
+++ views/client/block/rightmenu.php
@@ -1,53 +1,37 @@
-<?php
-use yii\helpers\Url;
-
-?>
 <div id="rightmenu">
     <ul>
         <li style="background: url('/images/icons/edit.gif') no-repeat 0px 6px;">
-            <a href="/account/create?parentId=<?= $account->contract_id ?>">Создать доп. ЛС</a>
+            <a href="/account/edit?id=<?= $account->id ?>">Редактир. ЛС</a>
         </li>
-    </ul>
-
-    <ul>
         <li style="background: url('/images/icons/edit.gif') no-repeat 0px 6px;">
-            <a href="/account/edit?id=<?= $account->id ?>">Редактировать ЛС</a>
+            <a href="/account/create?parentId=<?= $account->id ?>">Создать доп. ЛС</a>
         </li>
-        <li style="background: url('/images/icons/envelope.gif') no-repeat 0px 6px;">
-            <a href="/document/print-envelope?clientId=<?= $account->id ?>" target="_blank">Напечатать конверт</a>
-        </li>
-        <li style="background: url('/images/icons/printer.gif') no-repeat 0px 6px;">
-            <a href="?module=newaccounts&action=make_1c_bill&tty=mounting_orders">Заказ на Установку/Монтаж</a>
-        </li>
-        <li style="background: url('/images/icons/add.gif') no-repeat 0px 6px;">
-            <a href="?module=newaccounts&action=make_1c_bill&tty=shop_orders">Создать заказ из Магазина</a>
-        </li>
-        <li style="background: url('/images/icons/disable.gif') no-repeat 0px 6px;">
-            <a href="?module=newaccounts&action=make_1c_bill&tty=shop_orders&is_rollback=1">Возврат товара</a>
-        </li>
-        <li style="background: url('/images/icons/add.gif') no-repeat 0px 6px;">
-            <a href="?module=incomegoods&action=order_edit&id=">Создать заказ Поставщику</a>
-        </li>
-    </ul>
-
-    <ul>
-        <li>
+        <li style="background: url('/images/icons/edit.gif') no-repeat 0px 6px;">
             <a href="?module=tt&action=view_type&type_pk=2&show_add_form=true">Создать задание</a>
         </li>
-        <li>
-            <a href="?module=tt&action=view_type&type_pk=1&show_add_form=true">Создать заявку на поддержку</a>
+        <li style="background: url('/images/icons/edit.gif') no-repeat 0px 6px;">
+            <a href="?module=tt&action=view_type&type_pk=1&show_add_form=true">Создать трабл</a>
         </li>
-    </ul>
-
-    <ul>
+        <li style="background: url('/images/icons/edit.gif') no-repeat 0px 6px;">
+            <a href="/transfer/index/?client=<?= $account->id ?>" onclick="return showIframePopup(this)">Перенос услуг</a>
+        </li>
+        <li style="background: url('/images/icons/add.gif') no-repeat 0px 6px;">
+            <a href="?module=newaccounts&action=make_1c_bill&tty=mounting_orders">Установка/Монтаж</a>
+        </li>
+        <li style="background: url('/images/icons/add.gif') no-repeat 0px 6px;">
+            <a href="?module=newaccounts&action=make_1c_bill&tty=shop_orders">Заказ И-М</a>
+        </li>
+        <li style="background: url('/images/icons/add.gif') no-repeat 0px 6px;">
+            <a href="?module=newaccounts&action=make_1c_bill&tty=shop_orders&is_rollback=1">Возврат товара</a>
+        </li>
+        <li style="background: url('/images/icons/disable.gif') no-repeat 0px 6px;">
+            <a href="?module=incomegoods&action=order_edit&id=">Заказ Поставщику</a>
+        </li>
         <li style="background: url('/images/icons/printer.gif') no-repeat 0px 6px;">
-            <a href="/custom-print/print-client/?id=<?= $account->id; ?>" onClick="return ImmediatelyPrint(this)">Печать карточки клиента</a>
+            <a href="/document/print-envelope?clientId=<?= $account->id ?>" target="_blank">Конверт</a>
         </li>
-    </ul>
-
-    <ul>
-        <li>
-            <a href="/transfer/index/?client=<?= $account->id; ?>" onClick="return showIframePopup(this)">Перенос услуг</a>
+        <li style="background: url('/images/icons/printer.gif') no-repeat 0px 6px;">
+            <a href="/custom-print/print-client/?id=<?= $account->id ?>" onclick="return ImmediatelyPrint(this)">Карточка</a>
         </li>
     </ul>
 </div>
