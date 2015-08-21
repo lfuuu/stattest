3760d37e01768eacb4efa548101f3ad38518916b
Roman Elistratov <romane@mcn.ru>
2015-08-21 16:12:46 +0300

    BIL-562. NewAccount. Fix error

--- views/client/create.php
+++ views/client/create.php
@@ -46,3 +46,4 @@ $contragent->formLang = $language;
 
 <script type="text/javascript" src="/js/behaviors/managers_by_contract_type.js"></script>
 <script type="text/javascript" src="/js/behaviors/organization_by_legal_type.js"></script>
+<script type="text/javascript" src="/js/behaviors/find-bik.js"></script>
