c320890f58d5c2d36a88301e45d40748c9992ff5
Roman Elistratov <romane@mcn.ru>
2015-08-31 19:19:18 +0300

    BIL-604. UsaveVirtpbx. Fix tariffs list

--- dao/services/VoipServiceDao.php
+++ dao/services/VoipServiceDao.php
@@ -4,7 +4,7 @@ namespace app\dao\services;
 
 use Yii;
 use app\classes\Singleton;
-use app\models\UsageVoip;
+use app\models\TariffVoip;
 use app\models\ClientAccount;
 
 class VoipServiceDao extends Singleton implements ServiceDao
@@ -12,13 +12,21 @@ class VoipServiceDao extends Singleton implements ServiceDao
 
     public function getPossibleToTransfer(ClientAccount $client)
     {
-        $now = new \DateTime();
-
         return
             UsageVoip::find()
                 ->client($client->client)
                 ->actual()
                 ->andWhere(['next_usage_id' => 0])
+                ->all();
+    }
+
+    public function getTariffsList(ClientAccount $client, $region)
+    {
+        return
+            TariffVoip::find()
+                ->where(['currency_id' => $client->currency])
+                ->andWhere(['connection_point_id' => $region])
+                ->andWhere(['!=', 'status', 'archive'])
                 ->all();
     }
 
