d6f8c8d224f2c2cd99cf29f0369cd787097251cc
sooonic <stepan.okhorzin@gmail.com>
2015-09-25 15:49:21 +0400

    BIL712 View closed bills

--- stat/classes/BalanceSimple.php
+++ stat/classes/BalanceSimple.php
@@ -85,7 +85,9 @@ class BalanceSimple
 
             where
                 client_id='.$params['client_id'].'
-                '.($params["is_multy"] && !$params["is_view_canceled"] ? " and (state_id is null or (state_id is not null and state_id !=21)) " : "").'
+                '
+            /*($params["is_multy"] && !$params["is_view_canceled"] ? " and (state_id is null or (state_id is not null and state_id !=21)) " : "").*/
+        .'
             order by
                 bill_date desc,
                 bill_no desc
