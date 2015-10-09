44ea19d3960c1ceffd5c2aea7660aebd909a7989
Roman Elistratov <romane@mcn.ru>
2015-10-09 12:57:14 +0300

    BIL-706. Troubles. Update helper

--- helpers/DateTimeZoneHelper.php
+++ helpers/DateTimeZoneHelper.php
@@ -11,17 +11,20 @@ class DateTimeZoneHelper extends \yii\helpers\FileHelper
 
     public static function getDateTime($date, $format = 'Y-m-d H:i:s')
     {
-        $timezone = isset(Yii::$app->user->identity) ? Yii::$app->user->identity->timezone_name : 'UTC';
-        $datetime = (new DateTime($date))->setTimezone(new DateTimeZone($timezone));
+        $datetime = (new DateTime($date))->setTimezone(new DateTimeZone(self::getUserTimeZone()));
         return $format !== false ? $datetime->format($format) : $datetime;
     }
 
     public static function setDateTime($date, $format = false)
     {
-        $timezone = isset(Yii::$app->user->identity) ? Yii::$app->user->identity->timezone_name : 'UTC';
-        $datetime = new DateTime($date, new DateTimeZone($timezone));
+        $datetime = new DateTime($date, new DateTimeZone(self::getUserTimeZone()));
         $datetime->setTimezone(new DateTimeZone('UTC'));
         return $format !== false ? $datetime->format($format) : $datetime;
+    }
+
+    private static function getUserTimeZone()
+    {
+        return isset(Yii::$app->user->identity) ? Yii::$app->user->identity->timezone_name : 'UTC';
     }
 
 }
