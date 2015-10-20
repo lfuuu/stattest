a11b64810b57a5a73518d4a42a622985fa1e13a1
Андреев Дмитрий <adima@mcn.ru>
2015-10-20 13:25:20 +0300

    BIL-841: доработки работы с отстойником

--- tests/run.sh
+++ tests/run.sh
@@ -2,4 +2,6 @@
 set -e
 
 codeception/bin/db migrate/recreate-db
-~/.composer/vendor/bin/codecept run --xml
+~/.composer/vendor/bin/codecept run unit --xml unit.xml
+~/.composer/vendor/bin/codecept run web --xml web.xml
+~/.composer/vendor/bin/codecept run func --xml func.xml
