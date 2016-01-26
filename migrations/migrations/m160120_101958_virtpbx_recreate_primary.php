194e9c48566d3abdf03b9120ede5eaa3ca4a5fb2
Roman Elistratov <romane@mcn.ru>
2016-01-26 11:18:33 +0300

    BIL-1135. Commands. VatsStatistic. Re-create migration w/o plain SQL

--- migrations/migrations/m160120_101958_virtpbx_recreate_primary.php
+++ migrations/migrations/m160120_101958_virtpbx_recreate_primary.php
@@ -4,17 +4,13 @@ class m160120_101958_virtpbx_recreate_primary extends \app\classes\Migration
 {
     public function up()
     {
-        $this->execute('
-            ALTER TABLE `virtpbx_stat`
-                DROP PRIMARY KEY,
-                ADD PRIMARY KEY (`client_id`, `usage_id`, `date`);
-        ');
+        $this->dropPrimaryKey('PRIMARY', 'virtpbx_stat');
+        $this->addPrimaryKey('PRIMARY_UPDATE', 'virtpbx_stat', ['client_id', 'usage_id', 'date']);
     }
 
     public function down()
     {
-        echo "m160120_101958_virtpbx_recreate_primary cannot be reverted.\n";
-
-        return false;
+        $this->dropPrimaryKey('PRIMARY', 'virtpbx_stat');
+        $this->addPrimaryKey('PRIMARY_UPDATE', 'virtpbx_stat', ['client_id', 'date']);
     }
 }
