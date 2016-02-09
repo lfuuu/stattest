e6dfb024a0b648e15ce877e80563df37cc60e623
Андреев Дмитрий <adima@mcn.ru>
2016-02-09 22:56:05 +0300

    BIL-1219: Передавать дату договора череp API партнера

--- dao/PartnerDao.php
+++ dao/PartnerDao.php
@@ -23,7 +23,12 @@ class PartnerDao extends Singleton
                 foreach(ClientAccount::findAll(['contract_id' => $cc->id]) as $a) {
                     $accounts[] = $a->id;
                 }
-                $contracts[] = ['id' => $cc->id, 'accounts' => $accounts];
+
+                $contracts[] = [
+                    'id' => $cc->id, 
+                    'date' => $cc->document ? $cc->document->contract_date : null,
+                    'accounts' => $accounts,
+                ];
             }
 
             $data[$superId]['contragents'][$c->id] = ['id' => $c->id, 'name' => $c->name, 'contracts' => $contracts];
