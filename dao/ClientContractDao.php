2956dfaad4b6489d527369cd7b059de71599dd45
Андреев Дмитрий <adima@mcn.ru>
2016-03-18 18:07:32 +0300

    BIL-1383: Доработки

---
+++ dao/ClientContractDao.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\dao;
+
+use app\classes\Singleton;
+use app\helpers\DateTimeZoneHelper;
+use app\models\ClientContract;
+use app\models\ClientDocument;
+
+class ClientContractDao extends Singleton
+{
+
+    /**
+     * @param ClientContract $contract
+     * @param \DateTime|null $date
+     * @return null|\app\models\ClientDocument
+     */
+    public function getContractInfo(ClientContract $contract, \DateTime $date = null)
+    {
+        if (!$date) {
+            $date = new \DateTime('now', new \DateTimeZone(DateTimeZoneHelper::TIMEZONE_DEFAULT));
+        }
+
+        $contractDoc = ClientDocument::find()
+            ->active()
+            ->contract()
+            ->andWhere(['contract_id' => $contract->id])
+            ->andWhere(['<=', 'contract_date', $date->format(\DateTime::ATOM)])
+            ->last();
+
+        return $contractDoc;
+    }
+
+}
