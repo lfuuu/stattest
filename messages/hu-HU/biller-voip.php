5f43393229d5b47cfa1d169faaa85d34c0a9b6af
Андреев Дмитрий <adima@mcn.ru>
2016-03-31 11:28:11 +0300

    BIL-1314: Венгерские переводы биллинга.

--- messages/hu-HU/biller-voip.php
+++ messages/hu-HU/biller-voip.php
@@ -2,23 +2,61 @@
 
 return [
 
-    'voip_monthly_fee_per_number' => 'Előfizetési díj {service} telefonszámra{date_range}',
+    'voip_group_local' => 'helyi mobil',
+    'voip_group_long_distance' => 'helyi',
+    'voip_group_international' => 'nemzetközi',
 
-    'voip_overlimit' => 'Havidíjban foglalt limiten felüli hívások díja {service}{date_range}',
+    'voip_connection' => '{tariff} tarifájú IP telefonszolgáltatás',
 
-    'voip_calls_payment' => 'Távközlési forgalmi díj {service} telefonszámra{date_range}',
+    // Client "bill_rename1" = Előfizetési díj
+    'voip_monthly_fee_per_number' => 'Előfizetési díj a {service} telefonszámra, {date_range}',
 
-    'voip_package_monfly_fee' => '{service} előfizetői csomagdíj{date_range}',
+    // Client "bill_rename1" = Szerződés szerinti szolgáltatásnyújtás díja
+    'voip_monthly_fee_per_number_custom' => '{service} telefonszámon nyújtott szolgáltatások {date_range}{by_agreement}',
 
-    'voip_sip_trunk_monfly_fee' => '{tariff} SIP trunk előfizetési díj{date_range}',
+    // Client "bill_rename1" = Előfizetési díj hangcsatornáért
+    'voip_monthly_fee_per_line' => 'Előfizetési díj {lines_number} hangcsatornáért a {service}telefonszámhoz, {date_range}' ,
+    // Yii::t plural not work
+    // https://github.com/yiisoft/yii2/issues/4259
 
-    'voip_group_calls_payment' => 'Távközlési forgalmi díj {service} telefonszámra{date_range}',
-    'voip_group_calls_payment_custom' => 'Távközlési forgalmi díj {service} telefonszámra{date_range}',
+    // Client "bill_rename1" = Szerződés szerinti szolgáltatásnyújtás
+    'voip_monthly_fee_per_line_custom' => 'Előfizetési díj {lines_number} hangcsatornáért a {service}telefonszámhoz {by_agreement}',
+    // Yii::t plural not work
+    // https://github.com/yiisoft/yii2/issues/4259
 
-    'voip_package_fee' => '{tariff} {service} {date_range}',
-    'voip_package_payment' => '{tariff} {service} {date_range}',
-    'voip_package_minpay' => '{tariff} {service} {date_range}',
+    'voip_overlimit' => 'Előfizetői díjcsomag szerinti limit túllépés a {service} telefonszámnál (helyi hívásirány) {date_range}',
 
-    'voip_operator_trunk_orig' => '{service} {date_range}',
-    'voip_operator_trunk_term' => '{service} {date_range}',
+    'voip_local_mobile_call_minpay' => 'Minimális befizetés a helyi mobil irányba kezdeményezett hívásokért a {service} telefonszámról, {date_range}',
+    'voip_local_mobile_call_payment' => '{service} telefonszámról kezdeményezett helyi mobil irányú hívások forgalmi díja, {date_range}',
+
+    'voip_long_distance_call_minpay' => 'Minimális befizetés a helyi irányba kezdeményezett hívásokért a {service} telefonszámról, {date_range}',
+    'voip_long_distance_call_payment' => '{service} telefonszámról kezdeményezett helyi irányú hívások forgalmi díja, {date_range}',
+
+    'voip_international_call_minpay' => 'Minimális befizetés a nemzetközi irányba kezdeményezett hívásokért a {service} telefonszámról, {date_range}',
+    'voip_international_call_payment' => '{service} telefonszámról kezdeményezett nemzetközi irányú hívások forgalmi díja, {date_range}',
+
+    'voip_group_minpay' => 'Minimális befizetés a ({group}) irányba kezdeményezett hívásokért a {service} telefonszámról, {date_range}',
+    'voip_group_payment' => '{service} telefonszámról kezdeményezett ({group}) irányú hívások forgalmi díja, {date_range}',
+
+    'voip_calls_minpay' => 'Minimális befizetés a {service} telefonszámról kezdeményezett hívásokért, {date_range}',
+    // Client "bill_rename1" = Forgalmi díj
+    'voip_calls_payment' => '{service} telefonszámról kezdeményezett hívások forgalmi díja, {date_range}',
+    // Client "bill_rename1" = Serződés szerinti szolgáltatás-nyújtás
+    'voip_calls_payment_custom' => '{service} telefonszámon nyújtott telefonhívás szolgáltatás {date_range}{by_agreement}',
+
+    // Client "bill_rename1" = Forgalmi díj
+    'voip_group_calls_payment' => '{service} telefonszámról indított hívások (helyi, helyközi, nemzetközi) forgalmi díja, {date_range}',
+    // Client "bill_rename1" = Serződés szerinti szolgáltatás-nyújtás forgalmi díja
+    'voip_group_calls_payment_custom' => '{service} telefonszámról indított hívások (helyi, helyközi, nemzetközi) forgalmi díja, {date_range}{by_agreement}',
+
+    'voip_package_monfly_fee' => 'Percdíj csomag díja a {service} telefonszámhoz, {date_range}',
+
+    'voip_sip_trunk_monfly_fee' => '{tariff} tarifa szerinti SIP trönk előfizetési díja, {date_range}',
+
+    'voip_package_fee' => '"{tariff}" díjcsomag előfizetési díja {service} telefonszámhoz, {date_range}',
+    'voip_package_payment' => '"{tariff}" díjcsomag forgalmi díja {service} telefonszámhoz,{date_range}',
+    'voip_package_minpay' => '"{tariff}" díjcsomag minimális befizetése a {service} telefonszámhoz{date_range}',
+
+    'voip_operator_trunk_orig' => 'Díjköteles forgalom operátor irányból a {service} trönkön, {date_range}',
+    'voip_operator_trunk_term' => 'Díjköteles forgalom operátor irányba a {service} trönkön, {date_range}',
 ];
--- messages/hu-HU/biller.php
+++ messages/hu-HU/biller.php
@@ -3,26 +3,44 @@
 return [
 
     // Base biller
-    'date_once' => ', {0, date, yyyy.MM.dd}.',
-    'date_range_month' => ' {0, date, yyyy.MM.dd} -{1, date, yyyy.MM.dd}',
-    'date_range_year' =>  ' {0, date, yyyy.MM.dd} -{1, date, yyyy.MM.dd}',
-    'date_range_full' =>  ' {0, date, yyyy.MM.dd} -{1, date, yyyy.MM.dd}',
+    'serial_number' => '(Sorszám: {value}',
+    'pledge' => '{value} előlege',
+    'date_once' => ', {0, date, dd}',
+
+    'date_range_month' => ' {0, date, yyyy.MM.dd}-{1, date,yyyy.MM.dd} periódusra',
+    'date_range_year' =>  ' {0, date, yyyy.MM.dd}-{1, date,yyyy.MM.dd} periódusra',
+    'date_range_full' =>  ' {0, date, yyyy.MM.dd}-{1, date,yyyy.MM.dd} periódusra',
+
+
+    'by_agreement' => ',{contract_date,date,dd MMM yyyy} dátumú {contract_no} számú szerződés szerint',
+
+    // SMS
+    'sms_service' => '{tariff} SMS kézbesítés, {date_range}',
+    'sms_monthly_fee' => '{tariff}, SMS kézbesítés előfizetési díja',
+
+    // E-mail
+    'email_service' => '{local_part}@{domain} postafiók karbantartása, {date_range} {by_agreement}',
+
+    // Extra
+    'extra_service' => '{tariff}{date_range}{by_agreement}',
+    'extra_service_itpark' => '{contract_date, date, dd MMM yyyy} dátumú {contract} számú szerződés szerint',
+
+    // Welltime
+    'welltime_service' => '{tariff}{date_range}',
 
     // VPBX
-    'vpbx_service' => '{tariff} virtuális alközpont előfizetési díja{date_range}',
-    'vpbx_over_disk_usage' => 'Tárhely túllépés{date_range}',
-    'vpbx_over_ports_count' => 'Portok számának a túllépése{date_range}',
-    'vpbx_over_ext_did_count' => 'Számának túllépése külső számok{date_range}',
+    'vpbx_service' => '{tariff}{date_range}',
+    'vpbx_over_disk_usage' => 'Tárhely túllépési díj, {date_range}',
+    'vpbx_over_ports_count' => 'Port mennyiség túllépési díj, {date_range}',
+    'vpbx_over_ext_did_count' => 'Külső operátori telefonszám bevezetés havidíja, {date_range}',
 
     //Call_chat
     'call_chat_service' => '{tariff}{date_range}',
 
-    '' => '{}',
-
-    'paypal_payment_description' => '{account} számú egyenleg feltőltése {sum} {currency}',
+    'paypal_payment_description' => '{account} ügyfélszámú egyenleg feltöltés {sum} {currency} összeggel',
     'RUB' => 'rub',
     'HUF' => 'Ft',
     'USD' => '$',
 
-    'incomming_payment' => 'Fizetés',
+    'incomming_payment' => 'Egyenleg feltöltés távközlési szolgáltatásokra',
 ];
