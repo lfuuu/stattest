57fa5413df91a7f1aacd112a65de23d265c22fd3
Mikhail Konyukhov (lynx) <lynx@mcn.ru>
2016-04-12 14:37:57 +0300

    [+] service-json, fix reviw

--- controllers/api/OpenController.php
+++ controllers/api/OpenController.php
@@ -14,6 +14,29 @@ final class OpenController extends BaseController
 	Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
     }
 
+    /**
+     * @SWG\Get(
+     *   tags={"Выбрать список свободных номеров по одному региону"},
+     *   path="/api/open/",
+     *   summary="Выбрать список свободных номеров по одному региону",
+     *   operationId="Выбрать список свободных номеров по одному региону",
+     *   @SWG\Parameter(name="regions",type="integer[]",description="код региона",in="formData"),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="Выбрать список свободных номеров",
+     *     @SWG\Definition(
+     *       type="object[]"
+     *     )
+     *   ),
+     *   @SWG\Response(
+     *     response="default",
+     *     description="Ошибки",
+     *     @SWG\Schema(
+     *       ref="#/definitions/error_result"
+     *     )
+     *   )
+     * )
+     */
     public function actionGetFreeNumbers($region = null)
     {
         $numbers = Number::dao()->getFreeNumbersByRegion($region);
@@ -25,6 +48,29 @@ final class OpenController extends BaseController
         return $response;
     }
 
+    /**
+     * @SWG\Get(
+     *   tags={"Выбрать список свободных номеров по списку регинов"},
+     *   path="/api/open/",
+     *   summary="Выбрать список свободных номеров по списку регионов",
+     *   operationId="Выбрать список свободных номеров по списку регинов",
+     *   @SWG\Parameter(name="regions",type="integer[]",description="код региона",in="formData"),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="Выбрать список свободных номеров",
+     *     @SWG\Definition(
+     *       type="object[]"
+     *     )
+     *   ),
+     *   @SWG\Response(
+     *     response="default",
+     *     description="Ошибки",
+     *     @SWG\Schema(
+     *       ref="#/definitions/error_result"
+     *     )
+     *   )
+     * )
+     */
     public function actionGetFreeNumbersByList(array $regions = [])
     {
         $response = [];
@@ -32,7 +78,12 @@ final class OpenController extends BaseController
             $numbers = Number::dao()->getFreeNumbersByRegion($region);
 
             foreach($numbers as $r) {
-                $response []= [ "number" => $r->number, "beauty" => $r->beauty_level, "price" => $r->price, "region" => $r->region];
+                $response []= [
+		    "number" => $r->number,
+		    "beauty" => $r->beauty_level,
+		    "price" => $r->price,
+		    "region" => $r->region
+		];
             }
 	}
         return $response;
