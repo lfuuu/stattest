a5d22fee62c0a6bb85ed4171182796ae6778f4cc
Roman Elistratov <romane@mcn.ru>
2016-04-15 18:10:21 +0300

    BIL-1482. FreeNumbersFilter. Small update after review

--- models/filter/FreeNumberFilter.php
+++ models/filter/FreeNumberFilter.php
@@ -220,7 +220,7 @@ class FreeNumberFilter extends Number
         $union = clone $this->query;
 
         $this->query
-            ->andWhere(['number_cut' => mt_rand(0, 99)])
+            ->andWhere(['number_cut' => str_pad(mt_rand(0, 99), 2, 0, STR_PAD_LEFT)])
             ->union($union);
 
         return $this->one();
