21bef28b3b9e1cd30741466f76cf52066d203523
Roman Elistratov <romane@mcn.ru>
2016-04-21 11:30:04 +0300

    BIL-1482. FreeNumbersFilter. Append update trigger

--- migrations/migrations/m160415_114413_voip_numbers_prepare_rand.php
+++ migrations/migrations/m160415_114413_voip_numbers_prepare_rand.php
@@ -19,6 +19,12 @@ class m160415_114413_voip_numbers_prepare_rand extends \app\classes\Migration
                 SET NEW.number_cut = SUBSTRING(NEW.number, LENGTH(NEW.number)-1, 2);
             END
         ');
+
+        $this->execute('
+            CREATE TRIGGER update_voip_number BEFORE UPDATE ON voip_numbers FOR EACH ROW BEGIN
+                SET NEW.number_cut = SUBSTRING(NEW.number, LENGTH(NEW.number)-1, 2);
+            END
+        ');
     }
 
     public function down()
@@ -28,5 +34,6 @@ class m160415_114413_voip_numbers_prepare_rand extends \app\classes\Migration
         $this->dropIndex('voip_numbers_number_cut', $tblName);
         $this->dropColumn($tblName, 'number_cut');
         $this->execute('DROP TRIGGER IF EXISTS add_voip_number');
+        $this->execute('DROP TRIGGER IF EXISTS update_voip_number');
     }
 }
