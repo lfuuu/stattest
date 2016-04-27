d5599075ab4335571af6aec3caa160720e778ae4
mcnadima <dga@mcn.ru>
2016-04-27 13:18:04 +0300

    BIL-1508: В счетах не отображается название услуги ВАТС (#1933)

--- messages/ru-RU/biller.php
+++ messages/ru-RU/biller.php
@@ -29,7 +29,7 @@ return [
     'welltime_service' => '{tariff}{date_range}',
 
     // VPBX
-    'vpbx_service' => '{tariff}{date_range}',
+    'vpbx_service' => 'ВАТС {tariff}{date_range}',
     'vpbx_over_disk_usage' => 'Превышение дискового пространства{date_range}',
     'vpbx_over_ports_count' => 'Превышение количества портов{date_range}',
     'vpbx_over_ext_did_count' => 'Абонентская плата за услугу подключения номера стороннего оператора{date_range}',
