fe15fc28197b39e282398587fbd963ea5a30b643
BorisKorobkov <korobkov@mcn.ru>
2016-09-07 18:28:47 +0300

    BIL-1973, BIL-1977, BIL-1962 Для пакета с минутами каждый месяц создавать новую услугу

--- classes/behaviors/uu/SyncAccountTariffLight.php
+++ classes/behaviors/uu/SyncAccountTariffLight.php
@@ -2,106 +2,130 @@
 
 namespace app\classes\behaviors\uu;
 
-use app\classes\uu\model\AccountTariff;
-use app\classes\uu\model\AccountTariffLog;
+use app\classes\DateTimeWithUserTimezone;
+use app\classes\uu\model\AccountLogPeriod;
 use app\classes\uu\model\ServiceType;
 use app\modules\nnp\models\AccountTariffLight;
+use DateTimeZone;
 use yii\base\Behavior;
 use yii\base\Event;
 use yii\db\ActiveRecord;
 
-
+/**
+ * Синхронизировать данные в AccountTariffLight
+ * Не сразу, а через очередь - потому что в разных БД на разных серверах (mysql и postrgesql)
+ */
 class SyncAccountTariffLight extends Behavior
 {
+    const EVENT_ADD_TO_ACCOUNT_TARIFF_LIGHT = 'add_to_account_tariff_light';
+    const EVENT_DELETE_FROM_ACCOUNT_TARIFF_LIGHT = 'delete_from_account_tariff_light';
+
     public function events()
     {
         return [
-            ActiveRecord::EVENT_AFTER_INSERT => 'AccountTariffLogChange',
-            ActiveRecord::EVENT_AFTER_UPDATE => 'AccountTariffLogChange',
-            ActiveRecord::EVENT_AFTER_DELETE => 'AccountTariffLogChange',
+            ActiveRecord::EVENT_AFTER_INSERT => 'AccountLogPeriodChange',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'AccountLogPeriodChange',
+            ActiveRecord::EVENT_AFTER_DELETE => 'AccountLogPeriodDelete',
         ];
     }
 
     /**
-     * Триггер при изменении лога тарифов
+     * Триггер при изменении (добавлении/редактировании) списания абонентки
      * @param Event $event
      */
-    public function AccountTariffLogChange(Event $event)
+    public function AccountLogPeriodChange(Event $event)
     {
-        /** @var AccountTariffLog $accountTariffLog */
-        $accountTariffLog = $event->sender;
-        $accountTariff = $accountTariffLog->accountTariff;
-        $this->_sync($accountTariff);
-    }
-
-    /**
-     * Синхронизировать данные в AccountTariffLight
-     * @param AccountTariff $accountTariff
-     * @throws \Exception
-     */
-    protected function _sync(AccountTariff $accountTariff)
-    {
+        /** @var AccountLogPeriod $accountLogPeriod */
+        $accountLogPeriod = $event->sender;
+        $accountTariff = $accountLogPeriod->accountTariff;
         if ($accountTariff->service_type_id != ServiceType::ID_VOIP_PACKAGE) {
-            // пока только для пакетов
+            // только для пакетов
             return;
         }
 
-        // снаружи уже есть транзакция, но она на mysql, а тут на psql
-        $db = AccountTariffLight::getDb();
-        $transaction = $db->beginTransaction();
-        try {
+        $clientTimezoneName = $accountTariff->clientAccount->timezone_name;
+        $clientTimezone = new DateTimeZone($clientTimezoneName);
+        $utcTimezone = new DateTimeZone(DateTimeWithUserTimezone::TIMEZONE_UTC);
 
-            // исходные данные
-            $accountLogFromToTariffs = $accountTariff->getAccountLogHugeFromToTariffs(true);
+        $activateFrom = (new \DateTimeImmutable($accountLogPeriod->date_from, $clientTimezone))
+            ->setTimezone($utcTimezone)
+            ->format('U');
 
-            // ранее сконвертированные
-            /** @var AccountTariffLight[] $accountTariffLights */
-            $accountTariffLights = AccountTariffLight::find()
-                ->where(['account_client_id' => $accountTariff->client_account_id])
-                ->indexBy(function (AccountTariffLight $accountTariffLight) {
-                    return $accountTariffLight->activate_from . '_' . $accountTariffLight->tariff_id;
-                })
-                ->all();
+        $activateTo = (new \DateTimeImmutable($accountLogPeriod->date_to, $clientTimezone))
+            ->setTimezone($utcTimezone)
+            ->format('U');
 
-            // по всем исходным данным
-            foreach ($accountLogFromToTariffs as $accountLogFromToTariff) {
-
-                // должен быть такой же, как в indexBy при выборке $accountTariffLights!
-                $uniqueId = $accountLogFromToTariff->dateFrom->format('Y-m-d') . '_' . $accountLogFromToTariff->tariffPeriod->id;
-
-                if (isset($accountTariffLights[$uniqueId])) {
-                    $accountTariffLight = $accountTariffLights[$uniqueId];
-                    unset($accountTariffLights[$uniqueId]);
-                } else {
-                    $accountTariffLight = new AccountTariffLight;
-                }
-                if (!$accountTariff->prev_account_tariff_id) {
-                    throw new \LogicException('Универсальная услуга ' . $accountTariff->id . ' пакета телефонии не привязана к основной услуге телефонии');
-                }
-                $accountTariffLight->number = $accountTariff->prevAccountTariff->voip_number;
-                $accountTariffLight->account_client_id = $accountTariff->client_account_id;
-                $accountTariffLight->tariff_id = $accountLogFromToTariff->tariffPeriod->tariff_id;
-                $accountTariffLight->activate_from = $accountLogFromToTariff->dateFrom->format('Y-m-d');
-                $accountTariffLight->deactivate_from = $accountLogFromToTariff->dateTo ? $accountLogFromToTariff->dateTo->format('Y-m-d') : null;
-                if (!$accountTariffLight->save()) {
-                    throw new \Exception(implode(' ', $accountTariffLight->getFirstErrors()));
-                }
-
-            }
-
-            // оставшиеся ранее сконвертированные надо удалить
-            foreach ($accountTariffLights as $accountTariffLight) {
-                if (!$accountTariffLight->delete()) {
-                    throw new \Exception(implode(' ', $accountTariffLight->getFirstErrors()));
-                }
-            }
-
-            $transaction->commit();
-
-        } catch (\Exception $e) {
-            $transaction->rollBack();
-            throw $e;
+        if (!$accountTariff->prev_account_tariff_id) {
+            throw new \LogicException('Универсальная услуга ' . $accountTariff->id . ' пакета телефонии не привязана к основной услуге телефонии');
         }
 
+        \app\classes\Event::go(self::EVENT_ADD_TO_ACCOUNT_TARIFF_LIGHT, [
+                'id' => $accountLogPeriod->id,
+                'number' => $accountTariff->prevAccountTariff->voip_number,
+                'account_client_id' => $accountTariff->client_account_id,
+                'tariff_id' => $accountTariff->tariffPeriod->tariff_id,
+                'activate_from' => $activateFrom,
+                'deactivate_from' => $activateTo,
+            ]
+        );
+
+    }
+
+    /**
+     * Триггер при удалении списания абонентки
+     * @param Event $event
+     */
+    public function AccountLogPeriodDelete(Event $event)
+    {
+        /** @var AccountLogPeriod $accountLogPeriod */
+        $accountLogPeriod = $event->sender;
+        $accountTariff = $accountLogPeriod->accountTariff;
+        if ($accountTariff->service_type_id != ServiceType::ID_VOIP_PACKAGE) {
+            // только для пакетов
+            return;
+        }
+
+        \app\classes\Event::go(self::EVENT_DELETE_FROM_ACCOUNT_TARIFF_LIGHT, [
+                'id' => $accountLogPeriod->id,
+            ]
+        );
+
+    }
+
+    /**
+     * Добавить данные в AccountTariffLight
+     * @param array $params [id, number, account_client_id, tariff_id, activate_from, deactivate_from]
+     * @throws \Exception
+     * @internal param AccountLogPeriod $accountLogPeriod
+     */
+    public static function addToAccountTariffLight(array $params)
+    {
+        $accountTariffLight = AccountTariffLight::findOne(['id' => $params['id']]);
+        if (!$accountTariffLight) {
+            $accountTariffLight = new AccountTariffLight;
+            $accountTariffLight->id = $params['id'];
+        }
+        $accountTariffLight->number = $params['number'];
+        $accountTariffLight->account_client_id = $params['account_client_id'];
+        $accountTariffLight->tariff_id = $params['tariff_id'];
+        $accountTariffLight->activate_from = $params['activate_from'];
+        $accountTariffLight->deactivate_from = $params['deactivate_from'];
+        if (!$accountTariffLight->save()) {
+            throw new \Exception(implode(' ', $accountTariffLight->getFirstErrors()));
+        }
+    }
+
+    /**
+     * Удалить данные из AccountTariffLight. Теоретически этого быть не должно, но...
+     * @param array $params [id]
+     * @throws \Exception
+     * @internal param AccountLogPeriod $accountLogPeriod
+     */
+    public static function deleteFromAccountTariffLight(array $params)
+    {
+        $accountTariffLight = AccountTariffLight::findOne(['id' => $params['id']]);
+        if ($accountTariffLight && !$accountTariffLight->delete()) {
+            throw new \Exception(implode(' ', $accountTariffLight->getFirstErrors()));
+        }
     }
 }
