b79b10f7c4998e6e31c7da4fd2f49b9b6ef91c3f
Андреев Дмитрий <adima@mcn.ru>
2016-09-15 14:49:05 +0300

    Доработки

--- migrations/migrations/m160914_164618_sync_uu_voip.php
+++ migrations/migrations/m160914_164618_sync_uu_voip.php
@@ -31,18 +31,6 @@ class m160914_164618_sync_uu_voip extends \app\classes\Migration
 
         $this->alterColumn('z_sync_postgres', "tname", "enum('clients','usage_voip','usage_voip_package','tarifs_voip','log_tarif','usage_trunk','usage_trunk_settings','organization','prefixlist','tariff_package','dest_prefixes','currency_rate', '{$table}') CHARACTER SET utf8 COLLATE utf8_bin NOT NULL");
 
-        //процедуры в инсталяторе нет
-        $this->execute("DROP PROCEDURE IF EXISTS `z_sync_postgres`");
-        $this->execute("
-            CREATE PROCEDURE `z_sync_postgres`(IN p_table VARCHAR(20), IN p_id INTEGER(11))
-            BEGIN
-                DECLARE Continue HANDLER FOR 1062
-                BEGIN
-                    UPDATE z_sync_postgres SET rnd=RAND()*2000000000 WHERE tbase='nispd' and tname=p_table and tid=p_id;
-                END;
-                INSERT INTO z_sync_postgres(tbase, tname, tid, rnd) VALUES ('nispd', p_table, p_id, RAND()*2000000000);
-            END
-        ");
     }
 
     public function down()
@@ -52,6 +40,5 @@ class m160914_164618_sync_uu_voip extends \app\classes\Migration
         $this->execute("DROP TRIGGER IF EXISTS `sync_{$table}_voip_insert`");
         $this->execute("DROP TRIGGER IF EXISTS `sync_{$table}_voip_update`");
         $this->alterColumn('z_sync_postgres', "tname", "enum('clients','usage_voip','usage_voip_package','tarifs_voip','log_tarif','usage_trunk','usage_trunk_settings','organization','prefixlist','tariff_package','dest_prefixes','currency_rate') CHARACTER SET utf8 COLLATE utf8_bin NOT NULL");
-        $this->execute("DROP PROCEDURE IF EXISTS `z_sync_postgres`");
     }
 }
