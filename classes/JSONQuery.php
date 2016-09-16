f6983ad378f7dd7dd5beffb888deea950eced6ee
BorisKorobkov <korobkov@mcn.ru>
2016-09-16 15:01:04 +0300

    BIL-2018 Оптимизировать очередь

--- classes/JSONQuery.php
+++ classes/JSONQuery.php
@@ -1,17 +1,23 @@
 <?php
 namespace app\classes;
 
-use Yii;
 use Exception;
+use Yii;
 
 class JSONQuery
 {
+    /**
+     * @param string $url
+     * @param [] $data
+     * @param bool $isPostJSON
+     * @return mixed
+     * @throws Exception
+     */
     public static function exec($url, $data, $isPostJSON = true)
     {
-        Yii::info('Json request ' . $url . ': ' . json_encode($data,
-                JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        Yii::info('Json request ' . $url . ': ' . json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 
-        $defaults = array(
+        $defaults = [
             CURLOPT_POST => $isPostJSON ? 1 : 0,
             CURLOPT_HEADER => 0,
             CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
@@ -21,43 +27,48 @@ class JSONQuery
             CURLOPT_FORBID_REUSE => 1,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_SSL_VERIFYPEER => false
-        );
+        ];
 
         if ($data) {
             if ($isPostJSON) {
                 $defaults[CURLOPT_POSTFIELDS] = json_encode($data);
             } else {
-                $defaults[CURLOPT_URL] .= "?" . http_build_query($data);
+                $defaults[CURLOPT_URL] .= '?' . http_build_query($data);
             }
         }
 
-        //Event::go("json", [$url, $data], true);
+        //Event::go('json', [$url, $data], true);
+
+        $debugInfo = '';
+        $debugInfo .= sprintf('$url = %s', $url) . PHP_EOL;
+        $debugInfo .= sprintf('$data = %s', print_r($data, true)) . PHP_EOL;
+        $debugInfo .= sprintf('$isPostJSON = %d', $isPostJSON) . PHP_EOL;
 
         $ch = curl_init();
         curl_setopt_array($ch, $defaults);
         if (!$result = curl_exec($ch)) {
-            throw new Exception(curl_error($ch));
+            $debugInfo .= sprintf('curl_error = %s', curl_error($ch)) . PHP_EOL;
+            throw new Exception($debugInfo);
         }
 
         $info = curl_getinfo($ch);
         curl_close($ch);
 
-
-        if ($info["http_code"] !== 200) {
-            throw new Exception("Bad responce: http code: " . $info["http_code"], $info["http_code"]);
+        if ($info['http_code'] !== 200) {
+            $debugInfo .= sprintf('$info = %s', $info) . PHP_EOL;
+            throw new Exception($debugInfo, $info["http_code"]);
         }
 
         $response = $result;
         $result = @json_decode($result, true);
 
-
         if ($result === null) {
             Yii::info('Json response raw: ' . $response);
-            throw new Exception("Json decoding error", -1);
+            $debugInfo .= sprintf('$response = %s', $response) . PHP_EOL;
+            throw new Exception($debugInfo, -1);
         }
 
-        Yii::info('Json response: ' . json_encode($result,
-                JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        Yii::info('Json response: ' . json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 
         return $result;
     }
