aea29a745f3c83911a7ce88ddd6e5db46977021a
BorisKorobkov <korobkov@mcn.ru>
2016-09-30 13:55:39 +0300

    BIL-2010 Синхронизировать country в nnp

---
+++ modules/nnp/migrations/m160930_094922_fill_nnp_country.php
@@ -0,0 +1,18 @@
+<?php
+
+class m160930_094922_fill_nnp_country extends \app\classes\Migration
+{
+    public function up()
+    {
+        $this->execute("
+
+        ");
+    }
+
+    public function down()
+    {
+        echo "m160930_094922_fill_nnp_country cannot be reverted.\n";
+
+        return false;
+    }
+}
