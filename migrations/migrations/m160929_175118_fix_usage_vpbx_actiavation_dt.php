16356309e4922b53e1638ec5b0d1d23b2b44c04f
Андреев Дмитрий <adima@mcn.ru>
2016-10-02 16:07:49 +0300

    BIL-2083: Не указан activation_dt && expore_dt у некоторых услуг ВАТС

--- migrations/migrations/m160929_175118_fix_usage_vpbx_actiavation_dt.php
+++ migrations/migrations/m160929_175118_fix_usage_vpbx_actiavation_dt.php
@@ -6,13 +6,16 @@ class m160929_175118_fix_usage_vpbx_actiavation_dt extends \app\classes\Migratio
 {
     public function up()
     {
+        //нужно пересохранение, для точго что бы запустилось поведение, проставляющее activation_dt && expire_dt
         foreach (UsageVirtpbx::find()
                      ->where(['activation_dt' => null])
                      ->andWhere(['not', ['actual_from' => '0000-00-00']])
                      ->all() as $usage) {
 
-            //значение и так =1. Сохранение вызовет запуск поведения, которое проставит activation_dt && expire_dt
-            $usage->amount = 1;
+            $usage->comment .= ' ';
+            $usage->save();
+
+            $usage->comment = trim($usage->comment);
             $usage->save();
         }
     }
