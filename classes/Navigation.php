c6df7ee4712e2a211183d0b3c957f1e7959d59ee
Дмитрий <dga@mcn.ru>
2016-11-25 16:27:08 +0300

    Update Navigation.php

--- classes/Navigation.php
+++ classes/Navigation.php
@@ -98,7 +98,7 @@ class Navigation
                 ->addItem('Списки префиксов', ['/voip/prefixlist'], ['voip.access'])
                 ->addItem('Направления', ['/voip/destination'], ['voip.access'])
                 ->addItem('Тип номера', ['/voip/number-type'], ['voip.access'])
-                ->addItem('Номера', ['/voip/number'], ['voip.access'])
+                ->addItem('Номера', ['/voip/number'], ['stats.report'])
                 ->addItem('Реестр номеров', ['/voip/registry'], ['voip.access'])
         );
 
