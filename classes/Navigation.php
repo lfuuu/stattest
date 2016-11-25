535ff2fc0cfc36cf8ad1b4e7914d0d0ac3b1e197
Дмитрий <dga@mcn.ru>
2016-11-25 16:11:08 +0300

    Update Navigation.php

--- classes/Navigation.php
+++ classes/Navigation.php
@@ -55,7 +55,6 @@ class Navigation
                 ->addItem('Отчёт по файлам', ['/file/report'], ['stats.report'])
                 ->addItem('Отчет по OnLime', ['/reports/onlime-report'], ['stats.report'])
                 ->addItem('Отчет по OnLime оборудование', ['/reports/onlime-devices-report'], ['stats.report'])
-                ->addItem('Состояние номеров', ['/usage/number/detail-report'], ['stats.report'])
                 ->addItem('Себестоимость звонков', ['/report/voip/cost-report'], ['stats.report'])
         );
         $this->addBlockForStatModule('routers');
