f853b0782dfbe3867b7ac65ba1953ff7578d61ce
Boris Korobkov <boris@korobkov.su>
2016-12-26 16:17:44 +0400

    BIL-2428 УУ. Пакет. Прайслист (#2619)
    * BIL-2428 УУ. Пакет. Прайслист

--- views/uu/tariff/_editMainVoipPackagePricelist.php
+++ views/uu/tariff/_editMainVoipPackagePricelist.php
@@ -84,3 +84,23 @@ if ($editableType <= TariffController::EDITABLE_LIGHT) {
     )
     ?>
 </div>
+
+<script type='text/javascript'>
+    $(function () {
+        $("body")
+            .on("change", ".package-pricelist select", function () {
+                <!-- при изменении прайслиста вывести ссылку на его просмотр -->
+                <!-- событие надо ловить не только для нынешних select, но и для будущих -->
+                var $this = $(this);
+                var id = $this.attr("id").replace("-pricelist_id", "-id");
+                var $id = $("#" + id);
+                var $price = $id.next();
+                if (!$price.length) {
+                    $price = $("<a>").html("Цены").attr("target", "_blank").insertAfter($id);
+                }
+                $price.attr("href", "/index.php?module=voipnew&action=defs&pricelist=" + $this.val());
+            });
+        <!-- при инициализации прайслиста вывести ссылку на его просмотр -->
+        $(".package-pricelist select").trigger("change");
+    });
+</script>
