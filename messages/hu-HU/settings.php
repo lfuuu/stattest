c5fbfcedd3a13996cae0d1cf77b961517bb8ffec
Дмитрий <dga@mcn.ru>
2017-01-11 14:10:59 +0300

    BIL-2454: Поправить тему в венгерских письмах на события (#2639)
    * BIL-2454: Поправить тему в венгерских письмах на события
    
    * Update settings.php

--- messages/hu-HU/settings.php
+++ messages/hu-HU/settings.php
@@ -10,7 +10,7 @@ return [
     'email_subject_approve' => 'E-mail cím aktiválás az értesítésekhez',
     'email_subject_min_balance' => '{organization} értesítés: Kritikus egyenleg állapot',
     'email_subject_zero_balance' => '{organization} értesítés: Egyenleg keret túllépés - szolgáltatás korlátozás',
-    'email_subject_day_limit' => '{organization} értesítés: Превышение суточного лимита',
-    'email_subject_add_pay_notif' => '{organization} értesítés: Зачисление средств на лицевой счет',
+    'email_subject_day_limit' => '{organization} értesítés: Napi limit túllépés',
+    'email_subject_add_pay_notif' => '{organization} értesítés: Egyenleg feltöltés igazolás',
 
 ];
