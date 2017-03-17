3443ab54df17f37bdd51a727a2c73d9b1b760a58
Roman-Elistratov <romane@mcn.ru>
2017-03-17 12:06:02 +0300

    InlineJSCSS-HotFix. Iteration#1 (#2878)

--- web/views/client/block/main.js
+++ web/views/client/block/main.js
@@ -11,13 +11,20 @@
 
         $('.set-block').click(function (e) {
             e.stopPropagation();
-            var id = $(this).data('id');
-            t = $(this);
+
+            var id = $(this).data('id'),
+                $that = $(this);
+
             if (confirm('Вы уверены, что хотите ' + t.text().toLowerCase().trim() + ' ЛС № ' + id + '?')) {
-                if (t.hasClass('btn-danger')) {
-                    t.addClass('btn-success').removeClass('btn-danger').text('Заблокировать');
+                if ($that.hasClass('btn-danger')) {
+                    $that
+                        .addClass('btn-success')
+                        .removeClass('btn-danger').text('Заблокировать');
                 } else {
-                    t.addClass('btn-danger').removeClass('btn-success').text('Разблокировать');
+                    $that
+                        .addClass('btn-danger')
+                        .removeClass('btn-success')
+                        .text('Разблокировать');
                 }
 
                 location.href = '/account/set-block?id=' + id;
@@ -26,13 +33,23 @@
 
         $('.set-voip-disabled').click(function (e) {
             e.stopPropagation();
-            var id = $(this).data('id');
-            t = $(this);
-            if (confirm(t.hasClass('btn-danger') ? 'Выключить локальную блокировку' : 'Включить локальную блокировку')) {
-                if (t.hasClass('btn-danger')) {
-                    t.addClass('btn-success').removeClass('btn-danger').text('Лок. блок.').attr('title', 'Включить локальную блокировку');
+
+            var id = $(this).data('id'),
+                $that = $(this);
+
+            if (confirm($that.hasClass('btn-danger') ? 'Выключить локальную блокировку' : 'Включить локальную блокировку')) {
+                if ($that.hasClass('btn-danger')) {
+                    $that
+                        .addClass('btn-success')
+                        .removeClass('btn-danger')
+                        .text('Лок. блок.')
+                        .attr('title', 'Включить локальную блокировку');
                 } else {
-                    t.addClass('btn-danger').removeClass('btn-success').text('Лок. разблок.').attr('title', 'Выключить локальную блокировку');
+                    $that
+                        .addClass('btn-danger')
+                        .removeClass('btn-success')
+                        .text('Лок. разблок.')
+                        .attr('title', 'Выключить локальную блокировку');
                 }
 
                 location.href = '/account/set-voip-disable?id=' + id;
