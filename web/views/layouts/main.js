35437f5e900ea55d01ca1267249b52d2da324c72
Roman-Elistratov <romane@mcn.ru>
2017-03-24 16:17:04 +0300

    BIL-2858. PricelistReport. Fix main layout (#2907)

--- web/views/layouts/main.js
+++ web/views/layouts/main.js
@@ -1,38 +1,38 @@
+var LOADED = 1,
+    datepicker_ru = {
+        closeText: 'Закрыть',
+        prevText: '&#x3c;Пред',
+        nextText: 'След&#x3e;',
+        currentText: 'Сегодня',
+        monthNames: [
+            'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+        ],
+        monthNamesShort: [
+            'Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+            'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'
+        ],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'yy-mm-dd',
+        firstDay: 1,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+
+// перерендерить пришпиленную шапка таблицы
+window.reflowTableHeader = function () {
+    var $table = $('.kv-grid-table');
+    try {
+        $table.floatThead && $table.floatThead('reflow');
+    } catch (err) {
+    }
+};
+
 +function ($) {
     'use strict';
-
-    var LOADED = 1,
-        datepicker_ru = {
-            closeText: 'Закрыть',
-            prevText: '&#x3c;Пред',
-            nextText: 'След&#x3e;',
-            currentText: 'Сегодня',
-            monthNames: [
-                'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-                'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
-            ],
-            monthNamesShort: [
-                'Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
-                'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'
-            ],
-            dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-            dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-            dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-            weekHeader: 'Не',
-            dateFormat: 'yy-mm-dd',
-            firstDay: 1,
-            showMonthAfterYear: false,
-            yearSuffix: ''
-        };
-
-    // перерендерить пришпиленную шапка таблицы
-    window.reflowTableHeader = function () {
-        var $table = $('.kv-grid-table');
-        try {
-            $table.floatThead && $table.floatThead('reflow');
-        } catch (err) {
-        }
-    };
 
     $(function () {
         $('.panel-toggle-button')
