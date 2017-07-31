1378552054f8e30a88421ae3a38ba0f2e45ce51b
Дмитрий <dga@mcn.ru>
2017-07-31 11:22:54 +0300

    BIL-3299: en-EN => en-US (#3254)

--- messages/de-DE/biller.php
+++ messages/de-DE/biller.php
@@ -2,6 +2,6 @@
 
 use yii\helpers\ArrayHelper;
 
-return ArrayHelper::merge(include("../en-EN/biller.php"), [
+return ArrayHelper::merge(include("../en-US/biller.php"), [
     'Replenishment of the account {account} for the amount of {sum} {currency}' => 'Dem Kontoguthaben von {account} wurden {sum} {currency} hinzugefügt.'
 ]);
--- messages/sk-SK/biller.php
+++ messages/sk-SK/biller.php
@@ -2,6 +2,6 @@
 
 use yii\helpers\ArrayHelper;
 
-return ArrayHelper::merge(include("../en-EN/biller.php"), [
+return ArrayHelper::merge(include("../en-US/biller.php"), [
     'Replenishment of the account {account} for the amount of {sum} {currency}' => 'Na zákaznickom účte {account} bol dobytí kredit na sumu {sum} {currency}.'
 ]);
