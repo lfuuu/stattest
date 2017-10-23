cc4828a641995c9ef833a79c992bdf2d5c1909f4
Boris Korobkov <boris@korobkov.su>
2017-10-23 15:19:56 +0300

    Добавил мониторинги (#3498)

--- health/mttLog.sh
+++ health/mttLog.sh
@@ -5,4 +5,4 @@ logFile="/var/log/nispd/mtt/`date "+%Y.%m.%d"`.log"
 
 # Посчитать количество ошибок
 itemVal=`cat $logFile | grep Error | wc -l`
-./_echoJson.sh 'Mtt' $itemVal 1 10 100 # itemId itemVal warning critical error
+./_echoJson.sh 'MttLog' $itemVal 1 10 100 # itemId itemVal warning critical error
---
+++ health/mttProcess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Проверить, что mtt запущен
+
+if [ `ps aux | grep mtt\.sh | grep -v grep | wc -l` -lt 1 ]
+then
+    itemVal=1
+else
+    itemVal=0
+fi
+
+./_echoJson.sh 'MttProcess' $itemVal 1 1 1 # itemId itemVal warning critical error
