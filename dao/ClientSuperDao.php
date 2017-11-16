8427108f9d518f8fb47ab27b29018d0211a152aa
Дмитрий <dga@mcn.ru>
2017-11-16 13:32:45 +0300

    BIL-3673: Реструктуризация get-client-full-struct. Мониторинг. +2 метода (#3563)
    * BIL-3673: Реструктуризация get-client-full-struct. Мониторинг. +2 раздельных метода
    
    * BIL-3673: Типизация вывода

---
+++ dao/ClientSuperDao.php
@@ -0,0 +1,409 @@
+<?php
+
+namespace app\dao;
+
+use app\classes\helpers\ArrayHelper;
+use app\classes\Singleton;
+use app\helpers\DateTimeZoneHelper;
+use app\models\billing\Locks;
+use app\models\Business;
+use app\models\BusinessProcessStatus;
+use app\models\ClientAccount;
+use app\models\ClientContract;
+use app\models\ClientContragent;
+use app\models\ClientSuper;
+use app\models\Country;
+use app\modules\uu\models\ServiceType;
+use yii\base\InvalidParamException;
+
+/**
+ * @method static ClientSuperDao me($args = null)
+ */
+class ClientSuperDao extends Singleton
+{
+    const STRUCT_CLIENT_ALL = 'all';
+    const STRUCT_CLIENT_STRUCT = 'struct';
+
+    /**
+     * Возвращает массив структур клиента по id супер-клиентов
+     *
+     * @param $superIds
+     * @param string $type
+     * @return array
+     */
+    public function getSuperClientStructByIds($superIds, $type = self::STRUCT_CLIENT_ALL)
+    {
+        $fullResult = [];
+        foreach ($superIds as $id) {
+
+            $super = ClientSuper::findOne(['id' => $id]);
+            $accounts = ArrayHelper::index(
+                ClientAccount::find()
+                    ->where(['super_id' => $id])
+                    ->asArray()
+                    ->all(),
+                null,
+                'contract_id'
+            );
+
+            $contracts = ArrayHelper::index(
+                ClientContract::find()
+                    ->where(['super_id' => $id])
+                    ->asArray()
+                    ->all(),
+                null,
+                'contragent_id'
+            );
+
+            $contragents = ClientContragent::find()
+                ->where(['super_id' => $id])
+                ->asArray()
+                ->all();
+
+            $countries = Country::find()
+                ->where(['code' => array_unique(array_column($contragents, 'country_id'))])
+                ->indexBy('code')
+                ->all();
+
+
+            $timezone = DateTimeZoneHelper::TIMEZONE_MOSCOW;
+
+            $resultContragents = [];
+            $accountIdxs = [];
+
+            foreach ($contragents as $contragent) {
+                $resultContracts = [];
+                /** @var ClientContract $contract */
+                foreach ($contracts[$contragent['id']] as $contract) {
+                    $resultAccounts = [];
+                    /** @var ClientAccount $account */
+                    foreach ($accounts[$contract['id']] as $account) {
+                        $resultAccount = [
+                            'id' => $account['id'],
+                            'is_disabled' => $contract['business_process_status_id'] != BusinessProcessStatus::TELEKOM_MAINTENANCE_WORK,
+                            'is_blocked' => (bool)$account['is_blocked'],
+                            'is_finance_block' => null,
+                            'is_overran_block' => null,
+                            'is_bill_pay_overdue' => (bool)$account['is_bill_pay_overdue'],
+                            'is_postpaid' => (bool)$account['is_postpaid'],
+                            'credit' => (int)$account['credit'],
+                            'version' => $account['account_version'],
+                            'applications' => $this->_getPlatformaServicesCleaned($account)
+                        ];
+
+                        if ($type != self::STRUCT_CLIENT_ALL) {
+                            unset($resultAccount['is_finance_block'], $resultAccount['is_overran_block']);
+                        }
+
+                        $resultAccounts[] = $resultAccount;
+                        $timezone = $account['timezone_name'];
+                        $accountIdxs[$account['id']] = [
+                            'idx_account' => count($resultAccounts) - 1,
+                            'idx_contragent' => count($resultContragents),
+                            'idx_contract' => count($resultContracts)
+                        ];
+                    }
+
+                    if ($resultAccounts) {
+                        $resultContracts[] = [
+                            'id' => $contract['id'],
+                            'number' => $contract['number'],
+                            'state' => $contract['state'],
+                            'can_login_as_clients' => $contract['is_lk_access'],
+                            'partner_id' => $contragent['partner_contract_id'],
+                            'is_partner' => $contract['business_id'] == Business::PARTNER,
+                            'partner_login_allow' => $contract['is_partner_login_allow'],
+                            'business_id' => $contract['business_id'],
+                            'business_process_id' => $contract['business_process_id'],
+                            'business_process_status_id' => $contract['business_process_status_id'],
+                            'accounts' => $resultAccounts
+                        ];
+                    }
+                }
+
+                if ($resultContracts) {
+                    $resultContragents[] = [
+                        'id' => $contragent['id'],
+                        'name' => $contragent['name'],
+                        'country' => $countries && isset($countries[$contragent['country_id']]) ? $countries[$contragent['country_id']]->alpha_3 : '',
+                        'country_id' => $contragent['country_id'],
+                        'contracts' => $resultContracts
+                    ];
+                }
+            }
+
+            if ($resultContragents && $accountIdxs) {
+
+                if ($type == self::STRUCT_CLIENT_ALL) {
+                    $locksAnswer = $this->getAccountsLocks(null, array_keys($accountIdxs));
+
+                    if ($locksAnswer['is_load_complete']) {
+                        $locks = $locksAnswer['data'];
+
+                        foreach (array_keys($accountIdxs) as $accountId) {
+
+                            $lock = isset($locks[$accountId]) ? $locks[$accountId] : null;
+
+                            if (!isset($accountIdxs[$accountId])) {
+                                continue;
+                            }
+
+                            $idxContragent = $accountIdxs[$accountId]['idx_contragent'];
+                            $idxContract = $accountIdxs[$accountId]['idx_contract'];
+                            $idxAccount = $accountIdxs[$accountId]['idx_account'];
+
+                            $resultContragents[$idxContragent]['contracts'][$idxContract]['accounts'][$idxAccount]['is_finance_block'] = $lock && $lock['is_finance_block'];
+                            $resultContragents[$idxContragent]['contracts'][$idxContract]['accounts'][$idxAccount]['is_overran_block'] = $lock && $lock['is_overran_block'];
+                        }
+                    }
+                }
+
+                $fullResult[$super->id] = [
+                    'id' => $super->id,
+                    'timezone' => $timezone,
+                    'name' => $super->name,
+                    'contragents' => $resultContragents
+                ];
+            }
+        }
+
+        $this->_setTrueTypes($fullResult);
+
+        return $fullResult;
+    }
+
+    /**
+     * Получение блокировок в ЛС
+     * по супер-клиентам и ЛС
+     * @param array|null $superIds
+     * @param array|null $accountIds
+     * @return array
+     */
+    public function getAccountsLocks($superIds = null, $accountIds = null)
+    {
+        if (!$superIds && !$accountIds) {
+            throw new InvalidParamException('Параметры не заданны');
+        }
+
+        if (!$accountIds) {
+            $accountIds = array_column(
+                ClientAccount::find()
+                    ->where(['super_id' => $superIds])
+                    ->select('id')
+                    ->asArray()
+                    ->all(),
+                'id');
+        }
+
+        if (!$accountIds) {
+            return [];
+        }
+
+        if (!is_array($accountIds)) {
+            $accountIds = [$accountIds];
+        }
+
+        $isLoadComplete = true;
+        try {
+            Locks::getDb()->createCommand('SET statement_timeout TO 3000')->execute();
+            $locks = Locks::find()
+                ->where(['client_id' => $accountIds])
+                ->indexBy('client_id')
+                ->all();
+        } catch (\Exception $e) {
+            \Yii::error($e);
+            $isLoadComplete = false;
+        }
+
+        $data = [];
+        foreach ($accountIds as $accountId) {
+            /** @var Locks $lock */
+            $lock = isset($locks[$accountId]) ? $locks[$accountId] : null;
+            $data[$accountId] = [
+                'account_id' => $accountId,
+                'is_finance_block' => $lock && $lock->is_finance_block,
+                'is_overran_block' => $lock && ($lock->is_overran || $lock->is_mn_overran)
+            ];
+        }
+
+        return ['is_load_complete' => $isLoadComplete, 'data' => $data];
+    }
+
+    /**
+     * Возвращает массив Id супер-клиента по известным параметрам
+     *
+     * (для функции get-full-client-struct)
+     *
+     * @param integer $superId
+     * @param integer $superName
+     * @param integer $contractId
+     * @param integer $contragentId
+     * @param integer $contragentName
+     * @param integer $accountId
+     * @return int[]
+     */
+    public function getSuperIds($superId = null, $superName = null, $contractId = null, $contragentId = null, $contragentName = null, $accountId = null)
+    {
+        if ($superId) {
+            return is_array($superId) ? $superId : [$superId];
+        }
+
+        if ($superName) {
+            return ClientSuper::find()
+                ->select('id')
+                ->andWhere(['name' => $superName])
+                ->column();
+        }
+
+        if ($contragentId) {
+            return ClientContragent::find()
+                ->select('super_id')
+                ->distinct()
+                ->where(['id' => $contragentId])
+                ->column();
+
+        }
+
+        if ($contragentName) {
+            return ClientContragent::find()
+                ->select('super_id')
+                ->where(['name' => $contragentName])
+                ->groupBy('super_id')
+                ->column();
+        }
+
+        if ($contractId) {
+            return ClientContract::find()
+                ->select('super_id')
+                ->distinct()
+                ->where(['id' => $contractId])
+                ->column();
+        }
+
+        if ($accountId) {
+            return ClientAccount::find()
+                ->select('super_id')
+                ->distinct()
+                ->where(['id' => $accountId])
+                ->column();
+        }
+
+        return [];
+    }
+
+    /**
+     * Возвращает массив продуктов и их статус у клиента
+     *
+     * @param array $client
+     * @return array
+     */
+    private function _getPlatformaServicesCleaned($client)
+    {
+        return array_map(function ($row) {
+            $row['id'] = (int)$row['id'];
+            $row['is_enabled'] = (bool)$row['is_enabled'];
+            return $row;
+        },
+            \Yii::$app->db->createCommand("
+                SELECT 
+                    `usage_voip`.`id` AS `id`,
+                    'phone' AS `name`,
+                    (CAST(NOW() AS DATE) BETWEEN `actual_from` AND `actual_to`) AS `is_enabled` 
+                FROM `usage_voip`  
+                WHERE client = :client
+                
+                UNION ALL
+                
+                SELECT 
+                    `uu_account_tariff`.id AS `id`, 
+                    'phone' AS `name`, 
+                    tariff_period_id IS NOT NULL 
+                FROM `uu_account_tariff` 
+                WHERE client_account_id = :account_id AND service_type_id = :voipServiceId
+                 
+                UNION ALL 
+                SELECT 
+                    `usage_virtpbx`.`id` AS `id`,
+                    'vpbx' AS `name`,
+                    (CAST(NOW() AS DATE) BETWEEN `actual_from` AND `actual_to`) AS `is_enabled` 
+                FROM `usage_virtpbx`
+                WHERE client = :client
+                  
+                UNION ALL
+                 
+                SELECT 
+                    `uu_account_tariff`.id AS `id`, 
+                    'vpbx' AS `name`, 
+                    tariff_period_id IS NOT NULL 
+                FROM `uu_account_tariff` 
+                WHERE client_account_id = :account_id AND service_type_id = :vpbxServiceId
+                 
+                UNION ALL 
+                
+                SELECT 
+                    `usage_call_chat`.`id` AS `id`,
+                    'feedback' AS `name`,
+                    (CAST(NOW() AS DATE) BETWEEN `actual_from` AND `actual_to`) AS `is_enabled` 
+                FROM `usage_call_chat`
+                WHERE client = :client
+                                 
+                UNION ALL 
+                
+                SELECT 
+                    `uu_account_tariff`.id AS `id`, 
+                    'feedback' AS `name`, 
+                    tariff_period_id IS NOT NULL 
+                FROM `uu_account_tariff` 
+                WHERE client_account_id = :account_id AND service_type_id = :callChatServiceId
+                
+        ", [
+                    ':client' => $client['client'],
+                    ':account_id' => $client['id'],
+                    ':voipServiceId' => ServiceType::ID_VOIP,
+                    ':vpbxServiceId' => ServiceType::ID_VPBX,
+                    ':callChatServiceId' => ServiceType::ID_CALL_CHAT,
+                ]
+            )->queryAll());
+    }
+
+    /**
+     * Типизация вывода
+     *
+     * @param array $data
+     */
+    private function _setTrueTypes(&$data)
+    {
+        foreach ($data as &$superClient) {
+            foreach ($superClient['contragents'] as &$contragent) {
+                $this->_setArrayValuesAsInteger($contragent, ['id', 'country_id']);
+
+                foreach ($contragent['contracts'] as &$contract) {
+                    $this->_setArrayValuesAsInteger($contract, ['id', 'partner_id', 'can_login_as_clients', 'partner_login_allow', 'business_id', 'business_process_id', 'business_process_status_id']);
+
+                    foreach ($contract['accounts'] as &$account) {
+                        $this->_setArrayValuesAsInteger($account, ['id', 'version']);
+
+                        foreach ($account['applications'] as &$application) {
+                            $this->_setArrayValuesAsInteger($application, ['id']);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Конвертируем значения масиива в integer
+     *
+     * @param array $array
+     * @param string[] $keys
+     */
+    private function _setArrayValuesAsInteger(&$array, $keys)
+    {
+        foreach ($keys as $key) {
+            if (isset($array[$key])) {
+                $array[$key] = (int)$array[$key];
+            }
+        }
+    }
+}
