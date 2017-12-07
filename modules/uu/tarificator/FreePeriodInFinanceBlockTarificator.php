9fa5126fd065ad313f71233637475ad96f185cbf
Boris Korobkov <boris@korobkov.su>
2017-12-07 11:58:22 +0300

    BIL-3774 Не списывать абонплату / минималку (#3617)

--- modules/uu/tarificator/FreePeriodInFinanceBlockTarificator.php
+++ modules/uu/tarificator/FreePeriodInFinanceBlockTarificator.php
@@ -2,27 +2,20 @@
 
 namespace app\modules\uu\tarificator;
 
-use app\models\ClientAccount;
+use app\helpers\DateTimeZoneHelper;
 use app\models\important_events\ImportantEvents;
 use app\models\important_events\ImportantEventsNames;
-use app\models\usages\UsageInterface;
 use app\modules\uu\models\AccountLogMin;
 use app\modules\uu\models\AccountLogPeriod;
 use app\modules\uu\models\AccountTariff;
+use app\modules\uu\models\Period;
 use app\modules\uu\models\Tariff;
 use app\modules\uu\models\TariffPeriod;
+use DateTimeImmutable;
 use Yii;
 
 /**
- * Не списывать абонентку и минималку при финансовой блокировке
- * Ибо ЛС все равно не может пользоваться услугами
- *
- *  Надо следить, чтобы не возникала ситуация:
- * - списали абонентку в минус
- * - клиент ушел в финансовую блокировку
- * - отменили абонентку
- * - клиент вышел из финансовой блокировки
- * - дальше либо сказка про белого бычка, либо клиент пользуется услугой бесплатно
+ * Не списывать абонентку и минималку за ВТОРОЙ и последующие периоды при финансовой блокировке
  */
 class FreePeriodInFinanceBlockTarificator extends Tarificator
 {
@@ -38,50 +31,74 @@ class FreePeriodInFinanceBlockTarificator extends Tarificator
         $tariffTableName = Tariff::tableName();
         $importantEventsTableName = ImportantEvents::tableName();
 
-        // найти все даты установки фин.блокировки
-        // для каждой найти дату ее снятия (минимальную больше заданной), но ее может и не быть (тогда ее считаем максимальной)
+        // найти ЛС, у которых сейчас фин.блокировка
+        // если менее 2х дней назад - не учитывать
+        // если более 2х дней назад - только для посуточных тарифов
+        // если более 2х календарных месяцев назад - для помесячных тарифов
+        // для тарифов по годам можно не проверять - менеджер такое раньше сам заблокирует/отключит
         $sql = <<<SQL
-            CREATE TEMPORARY TABLE set_unset_zero_tmp
+            CREATE TEMPORARY TABLE set_zero_tmp
             SELECT
-                set_zero.client_id AS client_id,
-                DATE(DATE_ADD(set_zero.`date`, INTERVAL 1 DAY)) AS date_set_zero,
-                COALESCE(
+                client_id,
+                MAX(`date`) <= :min_month_date AS is_month
+            FROM
+            (
+                SELECT
+                    client_id,
+                    `date`,
                     (
                         SELECT
-                             MIN(DATE(unset_zero.`date`))
+                             MIN(unset_zero.`date`)
                         FROM
                             {$importantEventsTableName} unset_zero
                         WHERE
                             unset_zero.client_id = set_zero.client_id
                             AND unset_zero.event = :unsetZeroBalance
                             AND unset_zero.`date` >= set_zero.`date`
-                    ),
-                    :max_date
-                ) AS date_unset_zero
-                
-            FROM
-                {$importantEventsTableName} set_zero
-               
-            WHERE
-                set_zero.event = :setZeroBalance 
+                    ) AS unset_zero_date
+                FROM
+                    {$importantEventsTableName} set_zero
+                   
+                WHERE
+                    event = :setZeroBalance 
+                    AND `date` <= :min_day_date
+            ) t
             
+            WHERE
+                unset_zero_date IS NULL
             GROUP BY
-                set_zero.client_id,
-                set_zero.`date`
+                client_id
 SQL;
 
         $count = $db->createCommand($sql, [
-            ':max_date' => UsageInterface::MAX_POSSIBLE_DATE,
+            ':min_day_date' => (new DateTimeImmutable())
+                ->modify('-2 days')// 2, чтобы гарантировать для любой таймзоны
+                ->format(DateTimeZoneHelper::DATETIME_FORMAT),
+            ':min_month_date' => (new DateTimeImmutable())
+                ->modify('first day of previous month')
+                ->modify('+1 day')// чтобы гарантировать для любой таймзоны
+                ->format(DateTimeZoneHelper::DATETIME_FORMAT),
             ':setZeroBalance' => ImportantEventsNames::ZERO_BALANCE,
             ':unsetZeroBalance' => ImportantEventsNames::UNSET_ZERO_BALANCE,
-        ])->execute();
-        $this->out('Периодов фин.блокировки = ' . $count . PHP_EOL);
+        ])
+            ->execute();
+        $this->out('ЛС в фин.блокировке = ' . $count . PHP_EOL);
+
+        $params = [
+            ':day' => Period::ID_DAY,
+            ':month' => Period::ID_MONTH,
+            ':min_day_date' => (new DateTimeImmutable())
+                ->format(DateTimeZoneHelper::DATETIME_FORMAT),
+            ':min_month_date' => (new DateTimeImmutable())
+                ->modify('first day of this month')
+                ->format(DateTimeZoneHelper::DATETIME_FORMAT),
+        ];
 
         // сбросить абонентскую плату за периоды блокировки
         $accountLogPeriodTableName = AccountLogPeriod::tableName();
         $sql = <<<SQL
             UPDATE
-                set_unset_zero_tmp,
+                set_zero_tmp,
                 {$accountTariffTableName} account_tariff,
                 {$accountLogPeriodTableName} account_log_period,
                 {$tariffPeriodTableName} tariff_period,
@@ -89,22 +106,26 @@ SQL;
             SET
                 account_log_period.price = 0
             WHERE
-                set_unset_zero_tmp.client_id = account_tariff.client_account_id
+                set_zero_tmp.client_id = account_tariff.client_account_id
                 AND account_tariff.id = account_log_period.account_tariff_id
-                AND account_log_period.date_from > set_unset_zero_tmp.date_set_zero
-                AND account_log_period.date_to < set_unset_zero_tmp.date_unset_zero
+                AND 
+                    (
+                        (tariff_period.charge_period_id = :day AND account_log_period.date_from >= :min_day_date)
+                        OR 
+                        (set_zero_tmp.is_month AND tariff_period.charge_period_id = :month AND account_log_period.date_from >= :min_month_date)
+                    )
                 AND account_log_period.tariff_period_id = tariff_period.id
                 AND tariff_period.tariff_id = tariff.id
                 AND tariff.is_charge_after_blocking = 0
 SQL;
-        $count = $db->createCommand($sql)->execute();
+        $count = $db->createCommand($sql, $params)->execute();
         $this->out('Абонентка сброшена = ' . $count . PHP_EOL);
 
         // сбросить минимальную плату за периоды блокировки
         $accountLogMinTableName = AccountLogMin::tableName();
         $sql = <<<SQL
             UPDATE
-                set_unset_zero_tmp,
+                set_zero_tmp,
                 {$accountTariffTableName} account_tariff,
                 {$accountLogMinTableName} account_log_min,
                 {$tariffPeriodTableName} tariff_period,
@@ -112,20 +133,24 @@ SQL;
             SET
                 account_log_min.price = 0
             WHERE
-                set_unset_zero_tmp.client_id = account_tariff.client_account_id
+                set_zero_tmp.client_id = account_tariff.client_account_id
                 AND account_tariff.id = account_log_min.account_tariff_id
-                AND account_log_min.date_from > set_unset_zero_tmp.date_set_zero
-                AND account_log_min.date_to < set_unset_zero_tmp.date_unset_zero
+                AND 
+                    (
+                        (tariff_period.charge_period_id = :day AND account_log_min.date_from >= :min_day_date)
+                        OR 
+                        (set_zero_tmp.is_month AND tariff_period.charge_period_id = :month AND account_log_min.date_from >= :min_month_date)
+                    )
                 AND account_log_min.tariff_period_id = tariff_period.id
                 AND tariff_period.tariff_id = tariff.id
                 AND tariff.is_charge_after_blocking = 0
 SQL;
-        $count = $db->createCommand($sql)->execute();
+        $count = $db->createCommand($sql, $params)->execute();
         $this->out('Минималка сброшена = ' . $count . PHP_EOL);
 
         // убрать за собой
         $sql = <<<SQL
-            DROP TEMPORARY TABLE set_unset_zero_tmp
+            DROP TEMPORARY TABLE set_zero_tmp
 SQL;
         $db->createCommand($sql)->execute();
     }
