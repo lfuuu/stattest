ce792609c85ae6e317a00fb1e1324a6d44a741d8
Дмитрий <dga@mcn.ru>
2018-01-25 12:08:18 +0300

    BIL-3904: В Отчете calls_raw не работает группировка по типу номера (#3711)

--- classes/traits/CallsRawSlowReport.php
+++ classes/traits/CallsRawSlowReport.php
@@ -235,18 +235,18 @@ trait CallsRawSlowReport
             ->reportCondition('cr.nnp_country_code', $this->src_countries_ids);
 
 
-        $isSrcNdcTypeGroup = in_array('src_ndc_type_id', $this->group) ? true : false;
-        $isDstNdcTypeGroup = in_array('dst_ndc_type_id', $this->group) ? true : false;
+        $isSrcNdcTypeGroup = in_array('src_ndc_type_id', $this->group);
+        $isDstNdcTypeGroup = in_array('dst_ndc_type_id', $this->group);
 
         $query1 = $this->setDestinationCondition($query1, $query3, $this->dst_destinations_ids, $this->dst_number_type_ids, 'cr.nnp_number_range_id', $isDstNdcTypeGroup, 'dst');
         $query2 = $this->setDestinationCondition($query2, $query3, $this->src_destinations_ids, $this->src_number_type_ids, 'cr.nnp_number_range_id', $isSrcNdcTypeGroup, 'src');
 
         if ($isDstNdcTypeGroup || $this->dst_destinations_ids || $this->dst_number_type_ids) {
-            $query1->addSelect(['dst_ndc_type_id' => 'dst_nrd.ndc_type_id']);
+            $query1->addSelect('dst_ndc_type_id');
         }
 
         if ($isSrcNdcTypeGroup || $this->src_destinations_ids || $this->src_number_type_ids) {
-            $query2->addSelect(['src_ndc_type_id' => 'src_nrd.ndc_type_id']);
+            $query2->addSelect('src_ndc_type_id');
         }
 
         if ($this->is_success_calls) {
