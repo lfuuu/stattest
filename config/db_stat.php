81aad095653afa976e8c6d786b1f42cd0ffe1bfa
Дмитрий <dga@mcn.ru>
2018-02-19 17:40:17 +0300

    Кеширование схемы таблиц. (#3789)
    добавить в скрипт деплоя: rm -rf $STAT_DIR/runtime/cache/*

--- config/db_stat.php
+++ config/db_stat.php
@@ -8,4 +8,7 @@ return [
     'password' => 'vagrant',
     'charset' => 'utf8',
     'initQuery' => "SET @@session.time_zone = '+00:00';",
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
 ];
