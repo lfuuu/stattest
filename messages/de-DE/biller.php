3ceae0bb2917b13bb6ca60f5a14d9d05d2b50a6e
Дмитрий <dga@mcn.ru>
2018-02-26 15:52:24 +0300

    BIL-4044: Частичный перевод на немецкий (#3800)

--- messages/de-DE/biller.php
+++ messages/de-DE/biller.php
@@ -4,5 +4,16 @@ use app\models\Language;
 use yii\helpers\ArrayHelper;
 
 return ArrayHelper::merge(include(realpath(__DIR__ . '/../' . Language::LANGUAGE_ENGLISH . '/biller.php')), [
-    'Replenishment of the account {account} for the amount of {sum} {currency}' => 'Dem Kontoguthaben von {account} wurden {sum} {currency} hinzugefügt.'
+    'date_range_month' => ' ab {0, date, dd} bis {1, date, dd MMMM}',
+    'date_range_year' => ' ab {0, date, dd MMMM} Jahre bis {1, date, dd MMMM yyyy} Jahre',
+    'date_range_full' => ' ab {0, date, dd MMMM yyyy} Jahre bis {1, date, dd MMMM yyyy} Jahre',
+
+    'by_agreement' => ', nach Vereinbarung {contract_no} bis {contract_date,date,dd MMMM yyyy} Jahre',
+    'Replenishment of the account {account} for the amount of {sum} {currency}' => 'Guthaben aufgeladen für Account {account} {sum} {currency}',
+
+    'incomming_payment' => 'Prepayment für Telekommunikationsdienstleistungen',
+
+    'Communications services contract #{contract_number}' => 'Telekommunikationsdienstleistungen №{contract_number}',
+
+    'Month' => 'Monat',
 ]);
