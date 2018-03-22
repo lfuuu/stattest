ba115445792162ba5eee8e18110b23509a8bf362
Boris Korobkov <boris@korobkov.su>
2018-03-22 16:15:25 +0300

    Перенес json в отдельную папку (#3889)

--- health/exportFreeNumbers.sh
+++ health/exportFreeNumbers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Мониторинг экспорта свободных номеров (см. modules/platform)
-file='../web/free-number/number.tsv.gz'
+file='../web/export/free-number/number.tsv.gz'
 
 # declare, чтобы было числовое значение, которое инвертируем. То есть проверяем, что кол-во строк БОЛЬШЕ 50К
 declare -i itemVal=50000-`cat $file | gunzip | wc -l`
