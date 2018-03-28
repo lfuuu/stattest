06ad1a5be1b1f7809c9c87faed7ced040836fa6a
Boris Korobkov <boris@korobkov.su>
2018-03-28 16:57:18 +0300

    Экспорт свободных номеров (#3905)

--- modules/freeNumber/export.sh
+++ modules/freeNumber/export.sh
@@ -2,4 +2,6 @@
 ../../yii freeNumber/export/city | gzip > ../../web/export/free-number/city.tsv.gz;
 ../../yii freeNumber/export/country | gzip > ../../web/export/free-number/country.tsv.gz;
 ../../yii freeNumber/export/ndc-type | gzip > ../../web/export/free-number/ndc-type.tsv.gz;
-../../yii freeNumber/export/number | gzip > ../../web/export/free-number/number.tsv.gz;
+../../yii freeNumber/export/number | gzip > ../../web/export/free-number/number_tmp.tsv.gz;
+rm ../../web/export/free-number/number.tsv.gz;
+mv ../../web/export/free-number/number_tmp.tsv.gz ../../web/export/free-number/number.tsv.gz;
