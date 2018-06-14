3898b7cc9fbaa63eda05c3793402fa1380369236
Elias Wolzogen <37002058+wolzogen@users.noreply.github.com>
2018-06-14 11:32:16 +0300

    BIL-4290. Настройка параметров для MVNO (#4084)

--- modules/sim/config/params.php
+++ modules/sim/config/params.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'params' => [],
+    'params' => [
+        // Базовый URL для выполнения запросов
+        'base_url' => 'http://mvno-connector.mcn.ru/mttapi',
+        // API-ключ
+        'authorization' => '',
+        // Transfer MSISDN, через который происходит смена
+        'transfer_msisdn' => '',
+    ],
 ];
