32c0ab8d5280a743fc09576c1b0d7a6f1ff7d47c
Дмитрий <adima123@yandex.ru>
2018-08-15 12:13:56 +0300

    Правка лимитов мониторинга загрузки сервера стат (#4162)

--- health/loadAverage.sh
+++ health/loadAverage.sh
@@ -4,4 +4,4 @@
 loadAverageFloat=`awk '{print $2}' /proc/loadavg`
 LC_ALL=C loadAverageInt=$(printf '%.*f\n' 0 $loadAverageFloat) # bash не умеет оперировать вещественными числами. Дает либо ошибку, либо сравнивает посимвольно. Поэтому надо int. LC_ALL для разделителя точки, а не запятой
 
-./_echoJson.sh "LoadAverage"  $loadAverageInt 4 5 8 # itemId itemVal warning critical error
+./_echoJson.sh "LoadAverage"  $loadAverageInt 5 7 10 # itemId itemVal warning critical error
