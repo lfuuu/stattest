5154472c807b2dcb20e87102bda3e42224c987c2
Андреев Дмитрий <adima@mcn.ru>
2020-07-13 14:53:13 +0300

    move main postgresql host 235 to 252

--- config/db_pg_slave_cache.php
+++ config/db_pg_slave_cache.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'app\classes\Connection',
     'dsn' => '',
-    // 'dsn' => 'pgsql:host=85.94.32.235;port=5432;dbname=cdr', // eridanus.mcn.ru 85.94.32.235
+    // 'dsn' => 'pgsql:host=85.94.32.252;port=5432;dbname=cdr', // eridanus.mcn.ru 85.94.32.252
     // 'dsn' => 'pgsql:host=85.94.32.228;port=5432;dbname=cdr', // iberus.mcn.ru 85.94.32.228
     'charset' => 'utf8',
     'initQuery' => "SET SESSION TIME ZONE 'UTC';",
