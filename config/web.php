8e950ca58e48704a51fc7fc415051789cf37498c
ilya_shvedov <shvedov@mcn.ru>
2020-11-20 15:20:53 +0300

    BIL-5379 Stat to kub: step 10, add session migration

--- config/web.php
+++ config/web.php
@@ -5,6 +5,11 @@ use yii\web\Response;
 
 Yii::setAlias('@app', dirname(__DIR__));
 
+$session = require(__DIR__ . '/session.php');
+if (file_exists($file = __DIR__ . '/session.local.php')) {
+    $session = ArrayHelper::merge($session, require($file));
+}
+
 $cacheRedis = require(__DIR__ . '/cache_redis.php');
 if (file_exists($file = __DIR__ . '/cache_redis.local.php')) {
     $cacheRedis = ArrayHelper::merge($cacheRedis, require($file));
@@ -104,10 +109,7 @@ $config = [
     'language' => 'ru-RU',
     'timeZone' => 'UTC',
     'components' => [
-        'session' => [
-            'class' => 'yii\web\DbSession',
-            'cookieParams' => ['lifetime' => 40 * 60 * 60]
-        ],
+        'session' => $session,
         'view' => [
             'title' => 'stat - MCN Телеком',
             'class' => 'app\classes\BaseView',
