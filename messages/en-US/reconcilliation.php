402f6f63e18a8f912238b0df8e8a3f3ae13b1c94
itar3 <i.tarabykin@gmail.com>
2021-10-05 15:16:06 +0000

    Act of reconcilliation

---
+++ messages/en-US/reconcilliation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Reconcilliation act' => 'Reconcilliation Act',
+    'Description' => 'Of mutual payments as of {date} <br/> between {client_name} (personal account № {account_number}) <br/> and {company_name}.',
+    'According' => 'According to {name} $.',
+    'No' => '№ п/п',
+    'Debit' => 'Debit',
+    'Credit' => 'Credit',
+    'Documents' => 'Operation type,<br>documents',
+    'According to data' => 'According to {company_name} data as of {date},',
+    'Deposit' => 'including payments recieved in order to fulfill contract requirements:',
+    'Before debt' => ' ',
+    'Debt' => 'the amout of debt in favour of {company_name} составляет {deposit_balance} рублей.',
+    'No debt' => 'the amout of debt in favour of {client_name} составляет {deposit_balance} рублей.',
+    'Even' => 'there is no debt on each side.',
+    'From' => ' ',
+    'MP' => ' ',
+    'Currency' => '$',
+
+    'Head of oragnization' => 'Head of organization',
+    'Signature' => '(signature)',
+];  
---
+++ messages/ru-RU/reconcilliation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Reconcilliation act' => 'Акт Сверки',
+    'Description' => 'взаимных счетов по состоянию на {date} г. <br/> между {client_name} лицевой счет № {account_number} <br/> и {company_name}.',
+    'According' => 'По данным {name} руб.',
+    'No' => '№ п/п',
+    'Debit' => 'Дебет',
+    'Credit' => 'Кредит',
+    'Documents' => 'Наименование операции, <br/> документы',
+    'According to data' => 'По данным {company_name} на {date} г.,',
+    'Deposit' => 'с учетом платежей полученных в обеспечение исполнения обязательств по договору:',
+    'Before debt' => 'задолженность',
+    'Debt' => 'в пользу {company_name} составляет {deposit_balance} рублей.',
+    'No debt' => 'в пользу {client_name} составляет {deposit_balance} рублей.',
+    'Even' => 'отсутствует',
+    'From' => 'От',
+    'MP' => 'М.П.',
+    'Currency' => 'рублей',
+
+    'Head of oragnization' => 'Руководитель организации',
+    'Signature' => '(подпись)',
+];  
