849145b6157e6bd2c51f6982233db9f466e54b38
Андреев Дмитрий <adima@mcn.ru>
2022-03-11 18:25:28 +0300

    Deploy application components as needed

--- .helm/def.sh
+++ .helm/def.sh
@@ -11,13 +11,28 @@ function dev()
 	CI_URL="stat.mcn.loc"
   PGADMIN_IN_DEV="yes"
   export CI_DIR_HOME="/home/httpd/stat.mcn.ru"
+  export COUNTRY="RUS"
+
+  export is_with_phpmyadmin=1
+  export is_with_cryptopro=1
+  export is_with_comet=1
+  export is_with_graphql=1
+  export is_with_nnpported=0
 }
 
-function stage()
+function dev_hun()
 {
-	export ENVNAME=stage
-	CI_URL="$APPNAME-$ENVNAME.local"
-	export CI_DIR_HOME="/home/httpd/stat.mcn.local"
+	export ENVNAME=dev
+	CI_URL="stat.mcntelecom.loc"
+  PGADMIN_IN_DEV="yes"
+  export CI_DIR_HOME="/home/httpd/stat.mcn.ru"
+  export COUNTRY="HUN"
+
+  export is_with_phpmyadmin=1
+  export is_with_cryptopro=0
+  export is_with_comet=0
+  export is_with_graphql=0
+  export is_with_nnpported=0
 }
 
 function prod()
@@ -25,5 +40,26 @@ function prod()
 	export ENVNAME=prod
 	CI_URL="stat.mcn.ru"
 	export CI_DIR_HOME="/home/httpd/stat.mcn.ru"
+	export COUNTRY="RUS"
+
+  export is_with_phpmyadmin=0
+  export is_with_cryptopro=1
+  export is_with_comet=1
+  export is_with_graphql=1
+  export is_with_nnpported=1
+}
+
+function prod_hun()
+{
+	export ENVNAME=prod
+	CI_URL="stat.mcntele.com"
+	export CI_DIR_HOME="/home/httpd/stat.mcn.ru"
+	export COUNTRY="HUN"
+
+  export is_with_phpmyadmin=0
+  export is_with_cryptopro=0
+  export is_with_comet=1
+  export is_with_graphql=0
+  export is_with_nnpported=0
 }
 
