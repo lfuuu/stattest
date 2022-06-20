243bb3e9deeb4d5350290bf1e703117c7ed242c7
Андреев Дмитрий <adima@mcn.ru>
2022-06-20 11:30:46 +0300

    FIX: create client (EU)

--- classes/StatDbDriver.php
+++ classes/StatDbDriver.php
@@ -13,6 +13,19 @@ class StatDbDriver extends Singleton
      */
     public function getDbDriver()
     {
+        if (!defined('PATH_TO_ROOT')) {
+            define("PATH_TO_ROOT", \Yii::$app->basePath . '/stat/');
+        }
+
+        if (!defined("NO_WEB")) {
+            define("NO_WEB", 1);
+        }
+
+        if (!defined('DESIGN_PATH')) {
+            include_once PATH_TO_ROOT . 'conf.php';
+        }
+
+
         if ($this->db === null) {
             $this->db = new \MySQLDatabase_yii(Yii::$app->getDb());
         }
