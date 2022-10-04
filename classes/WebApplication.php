c5dc35b9b10baf3e885b2700e71c738e1c395017
Дмитрий <adima123@yandex.ru>
2022-10-04 19:33:08 +0300

    Update WebApplication.php

--- classes/WebApplication.php
+++ classes/WebApplication.php
@@ -136,6 +136,6 @@ class WebApplication extends \yii\web\Application
 
     public function is2fAuth()
     {
-        return false; // $this->isRus() && (getenv('IS_TEST') === "0") && ApiPhone::me()->isAvailable();
+        return $this->isRus() && (getenv('IS_TEST') === "0") && ApiPhone::me()->isAvailable();
     }
 }
