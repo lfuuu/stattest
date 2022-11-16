c66cbddb605e6ee888c82f6e7faa68eee7e22b22
Андреев Дмитрий <adima@mcn.ru>
2022-11-16 16:20:22 +0300

    STAT-361: API Vpbx. Enable_promter parametr

--- migrations/migrations/m210921_140950_vats_prompter.php
+++ migrations/migrations/m210921_140950_vats_prompter.php
@@ -13,7 +13,7 @@ class m210921_140950_vats_prompter extends \app\classes\Migration
         // throw new \Exception('Please start manually');
         $this->insertResource(
             \app\modules\uu\models\ServiceType::ID_VPBX,
-            \app\modules\uu\models\ResourceModel::ID_VPBX_PROMPTER, [
+            \app\modules\uu\models\ResourceModel::ID_VPBX_PROMTER, [
             'name' => 'Cуфлер',
             'unit' => '¤',
             'min_value' => 0,
