9563e0d64ffe53150f1ef4f313bd63c4365ad821
Андреев Дмитрий <adima@mcn.ru>
2023-01-17 16:49:54 +0300

    STAT-404: Payment owner recognition

--- classes/payments/recognition/PaymentOwnerRecognition.php
+++ classes/payments/recognition/PaymentOwnerRecognition.php
@@ -7,6 +7,7 @@ use app\classes\Singleton;
 use app\models\Bill;
 use app\models\ClientAccount;
 use app\models\ClientContract;
+use app\modules\uu\models\AccountTariff;
 
 class PaymentOwnerRecognition extends Singleton
 {
@@ -16,6 +17,7 @@ class PaymentOwnerRecognition extends Singleton
     public function who($model)
     {
         $this->logger = new LoggerSimpleInternal();
+        $this->listInnAccountIds = [];
 
         if (!$model->info_json) {
             $this->logger->add('Пустой info_json');
@@ -24,33 +26,31 @@ class PaymentOwnerRecognition extends Singleton
 
         $info = json_decode($model->info_json, true);
 
-        if (!isset($info['paymentPurpose']) || !$info['paymentPurpose']) {
+        if (!$info || !isset($info['paymentPurpose']) || !$info['paymentPurpose']) {
             return;
-//            $this->logger->add('Пустое описание');
+        }
+
+        $purpose = $info['paymentPurpose'];
+
+        $data = $this->parseDescription($purpose);
+
+        $descriptionAccountId = null;
+        if (!$data) {
+            $this->logger->add('В описании платежа ничего не распознано');
         } else {
-
-            $purpose = $info['paymentPurpose'];
-
-            $data = $this->parseDescription($purpose);
-
-            if (!$data) {
-                $this->logger->add('В описании платежа ничего не распознано');
-            } else {
-                $accountId = $this->accountQualifierByDescription($data);
-                if ($accountId) {
-                    $this->logger->add('Результат поиска по описанию: Л/С: ' . $accountId);
-                    return $accountId;
-                }
+            $descriptionAccountId = $this->accountQualifierByDescription($data);
+            if ($descriptionAccountId) {
+                $this->logger->add('Результат поиска по описанию: Л/С: ' . $descriptionAccountId);
             }
         }
 
-        $accountId = $this->accountQualifierByInn($info['payerInn']);
-        if ($accountId) {
-            $this->logger->add('Результат по ИНН: Л/С: ' . $accountId);
-            return $accountId;
+        $innAccountId = $this->accountQualifierByInn($info['payerInn'] ?? $data['inn'] ?? false, $descriptionAccountId);
+        if ($innAccountId) {
+            $this->logger->add('Результат: Л/С: ' . $innAccountId);
+            return $innAccountId;
         }
 
-        $this->logger->add('Результат: Л/С не найден');
+        $this->logger->add('Итого: Л/С не найден');
     }
 
     public function getLog()
@@ -65,21 +65,33 @@ class PaymentOwnerRecognition extends Singleton
 
         if (preg_match("/\D(20\d{4}\s*-\s*\d{6,7})\D?/", $description, $matches)) {
             $data['bill_no'] = preg_replace('/[^\d\-]+/', '', $matches[1]);
-            $this->logger->add('найден счет: ' . $data['bill_no']);
+            $this->logger->add('Скан: найден счет: ' . $data['bill_no']);
         }
 
         if (preg_match("/id(\d{5,})/", $description, $matches)) {
             $data['account_id'] = $matches[1];
-            $this->logger->add('найден Л/С: ' . $data['account_id']);
+            $this->logger->add('Скан: найден Л/С: ' . $data['account_id']);
         } elseif (preg_match("/(л\/с|лицевому счету)\s*(#|№| )?(\d{5,6})[^\d-]/iu", $description, $matches)) {
             $data['account_id'] = $matches[3];
-            $this->logger->add('найден Л/С: ' . $data['account_id']);
+            $this->logger->add('Скан: найден Л/С: ' . $data['account_id']);
         }
 
         if (preg_match("/(по\s*)?дог(овор(у|а)?)?\.?\s*(оказанаия)?\s*(услуг связи)?\s*№?#?\s*([0-9A-Z_\-]{5,})/iu", $description, $matches)) {
             $data['contract_number'] = $matches[6];
-            $this->logger->add('найден договор: ' . $data['contract_number']);
+            $this->logger->add('Скан: найден договор: ' . $data['contract_number']);
         }
+
+        if (preg_match("/ИНН\s*(\d{10})/", $description, $matches)) {
+            $data['inn'] = $matches[1];
+            $description = str_replace($data['inn'], '',  $description);
+            $this->logger->add('Скан: найден ИНН: ' . $data['inn']);
+        }
+
+        if (preg_match("/\+?((7|8)?(?'number'\d{10}))/", $description, $matches)) {
+            $data['voip_number'] = '7' . $matches['number'];
+            $this->logger->add('Скан: найден тел.номер: ' . $data['voip_number']);
+        }
+
 
         return $data;
     }
@@ -125,40 +137,109 @@ class PaymentOwnerRecognition extends Singleton
                     $this->logger->add('найден активный ЛС (' . $accountIds[0] . ') по договору');
                     return reset($accountIds);
                 }
-
             } else {
                 $this->logger->add('Договор не найден');
             }
         }
+
+        if (isset($data['voip_number']) && $data['voip_number']) {
+            $logPrefix = "Номер {$data['voip_number']}:";
+            /** @var AccountTariff $accountTariff */
+            $accountTariff = AccountTariff::find()->where(['voip_number' => $data['voip_number']])->andWhere(['NOT', ['tariff_period_id' => null]])->one();
+            if ($accountTariff) {
+                $this->logger->add("{$logPrefix} Найден ЛС {$accountTariff->client_account_id} по включенному номеру");
+                return $accountTariff->client_account_id;
+            }
+
+            $accountTariffs = AccountTariff::find()->where(['voip_number' => $data['voip_number']])->all();
+            if (count($accountTariffs) == 1) {
+                /** @var AccountTariff $firstAccountTariff */
+                $firstAccountTariff = reset($accountTariffs);
+                $this->logger->add("{$logPrefix} Найден ЛС {$firstAccountTariff->client_account_id} по единственному (отключенному) номеру");
+                return $firstAccountTariff->client_account_id;
+            } elseif ($accountTariffs) {
+                $this->logger->add("{$logPrefix} Найдено ЛС: " . count($accountTariffs) . " шт. Выбор неоднозначен.");
+            } else {
+                $this->logger->add("{$logPrefix} Не найден в услугах");
+            }
+        }
     }
 
-    public function accountQualifierByInn($inn)
+    public function accountQualifierByInn($inn, $descriptionAccountId)
     {
         if (!$inn) {
             $this->logger->add('ИНН не задан');
             return;
         }
 
-        $accountIds = ClientAccount::find()
+        if (!$descriptionAccountId) {
+            $accountIds = $this->getAccountIdsByInn($inn);
+            if (!$accountIds) {
+                $accountIds = $this->getAccountIdsByInn($inn, false);
+            }
+
+            if (count($accountIds) == 1) {
+                $accountId = reset($accountIds);
+                $this->logger->add("Найден единственный активный Л/С {$accountId} по ИНН: {$inn}");
+                return $accountId;
+            } elseif ($accountIds) {
+                $this->logger->add("Нет Л/С по ИНН {$inn}");
+                return ;
+            } else {
+                $this->logger->add('нет однозначного выбора ЛС: ' . implode(", ", array_splice($accountIds, 0, 10)) . (count($accountIds) > 10 ? '...' : ''));
+                return ;
+            }
+        }
+
+        $accountIds = $this->getAccountIdsByInn($inn, !((bool)$descriptionAccountId));
+
+        if ($accountIds) {
+            if (in_array($descriptionAccountId, $accountIds)) {
+                $this->logger->add("Л/С {$descriptionAccountId} ЕСТЬ в списке тех, у кого есть ИНН {$inn}");
+                return $descriptionAccountId;
+            } else {
+                $this->logger->add("Л/С {$descriptionAccountId} НЕТ в списке тех, у кого есть ИНН {$inn} (" . $this->getList($accountIds) . ")");
+                $this->logger->add("Требуется проверка");
+                return;
+            }
+        } else {
+            $this->logger->add("Нет Л/С по ИНН {$inn}");
+            $this->logger->add("Требуется проверка");
+            return;
+        }
+
+        if (count($accountIds) == 1) {
+            $accountId = reset($accountIds);
+            $this->logger->add("Найден единственный Л/С {$accountId} у ИНН: {$inn}");
+            return $accountId;
+        }
+
+        // $accountIds && count($accountIds) > 1
+        $this->logger->add('нет однозначного выбора ЛС: ' . implode(", ", array_splice($accountIds, 0, 10)) . (count($accountIds) > 10 ? '...' : ''));
+        return;
+    }
+
+    private function getAccountIdsByInn($inn, $isActive = true)
+    {
+        $query = ClientAccount::find()
             ->alias('c')
             ->joinWith('clientContractModel.clientContragent cg')
             ->where([
-                'c.is_active' => 1,
                 'cg.inn' => $inn
             ])->select('c.id')
-            ->limit(10)
-            ->column();
+            ->limit(100);
 
-        $this->logger->add('по ИНН ' . $inn . ' найдено Л/С: ' . count($accountIds) . ' шт.');
-        if (!$accountIds) {
-            return;
+        if ($isActive) {
+            $query->andWhere([
+                'c.is_active' => 1,
+            ]);
         }
 
-        if (count($accountIds) > 1) {
-            $this->logger->add('нет однозначного выбора ЛС: ' . implode(", ", $accountIds));
-            return;
-        }
+        return $query->column();
+    }
 
-        return reset($accountIds);
+    private function getList($array)
+    {
+        return implode(", ", array_splice($array, 0, 10)) . (count($array) > 10 ? '...' : '');
     }
 }
