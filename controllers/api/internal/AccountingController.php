35e530cb59bc3ebb77526dd300a4cdad48c0e351
Андреев Дмитрий <adima@mcn.ru>
2023-08-02 10:31:15 +0300

    API. Accounting. Current Statement.

---
+++ controllers/api/internal/AccountingController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\controllers\api\internal;
+
+use app\exceptions\web\NotImplementedHttpException;
+use app\modules\uu\models\Bill as uuBill;
+use Yii;
+use app\classes\Assert;
+use app\classes\ApiInternalController;
+use app\models\billing\Trunk;
+use app\models\billing\Server;
+
+class AccountingController extends ApiInternalController
+{
+    /**
+     * @throws NotImplementedHttpException
+     */
+    public function actionIndex()
+    {
+        throw new NotImplementedHttpException;
+    }
+
+    /**
+     * @SWG\Post(tags={"Accounting"}, path="/internal/accounting/get-current-statement/", summary="Получение текущей выписки", operationId="getCurrentStatement",
+     *   @SWG\Parameter(name="accountId", type="integer", description="ID ЛС", in="formData", default=""),
+     *
+     *   @SWG\Response(response=200, description="выписка",
+     *   ),
+     * )
+     */
+    public function actionGetCurrentStatement(int $accountId)
+    {
+        $lines = [];
+        $sum = 0;
+
+        $query = uuBill::getUnconvertedAccountEntries($accountId);
+        foreach ($query->each() as $uuLine) {
+            $lines[] = [
+                'item' => $uuLine->getFullName(),
+                'date_from' => '',
+                'amount' => 1,
+                'price' => number_format($uuLine->price_with_vat, 2, '.', ''),
+                'sum' => number_format($uuLine->price_with_vat, 2, '.', '')
+            ];
+
+            $sum += $uuLine['price_with_vat'];
+        }
+
+        return [
+            "bill" => [
+                "bill_no" => 'current_statement',
+                "is_rollback" => 0,
+                "is_1c" => 0,
+                "lines" => $lines,
+                "sum_total" => $sum,
+                "dtypes" => ['bill_no' => 'current_statement', 'ts' => time()]
+            ],
+            "link" => [
+            ],
+        ];
+    }
+}
