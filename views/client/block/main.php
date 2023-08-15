2edc32e2f52ff2dd5441413fbd30ff82c8e17434
Roman Khabarov <roman@globalart.dev>
2023-08-15 10:11:06 +0300

    feat: changed link to lk

--- views/client/block/main.php
+++ views/client/block/main.php
@@ -29,7 +29,7 @@ $priceLevels = PriceLevel::getList();
         </div>
         <div class="col-sm-4" class="c-blue-color">
             <?php if (ApiCore::isAvailable()) : ?>
-                    <a href="https://<?= Yii::$app->params['BASE_SERVER']; ?>/api/public/api/auth/login/support?accountId=<?= $account->id ?>"
+                    <a href="https://<?= Yii::$app->params['BASE_SERVER']; ?>/api/public/api/auth/login/support?accountId=<?= $account->id ?>&withRedirect=true"
                        target="_blank">
                         Переход в ЛК
                     </a>
