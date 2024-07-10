a090718aca1b8440a76a4a7830b5aef150f21402
Андреев Дмитрий <adima@mcn.ru>
2024-07-10 16:36:07 +0300

    SRM redirect log id

---
+++ migrations/migrations/m240710_132100_srm_redirect_idx.php
@@ -0,0 +1,64 @@
+<?php
+
+
+/**
+ * Class m240710_132100_srm_redirect_idx
+ */
+class m240710_132100_srm_redirect_idx extends \app\classes\Migration
+{
+    /**
+     * Up
+     */
+    public function safeUp()
+    {
+        $this->addColumn('sorm_redirect_ranges', 'id', $this->primaryKey());
+
+        $sql = <<<SQL
+CREATE TRIGGER sorm_redirect_ranges_after_ins_tr
+    after insert
+    on sorm_redirect_ranges
+    for each row
+BEGIN
+     insert into z_model_life_log (model, model_id, action, created_at) values ('sorm_redirect_ranges', NEW.id, 'insert', NOW());
+END;
+
+SQL;
+        $this->execute($sql);
+
+        $sql = <<<SQL
+CREATE TRIGGER sorm_redirect_ranges_after_upd_tr
+    after update
+    on sorm_redirect_ranges
+    for each row
+BEGIN
+     insert into z_model_life_log (model, model_id, action, created_at) values ('sorm_redirect_ranges', NEW.id, 'update', NOW());
+END;
+
+SQL;
+        $this->execute($sql);
+
+        $sql = <<<SQL
+CREATE TRIGGER sorm_redirect_ranges_before_upd_tr
+    before delete
+    on sorm_redirect_ranges
+    for each row
+BEGIN
+     insert into z_model_life_log (model, model_id, action, created_at) values ('sorm_redirect_ranges', OLD.id, 'delete', NOW());
+END;
+
+SQL;
+        $this->execute($sql);
+
+    }
+
+    /**
+     * Down
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('sorm_redirect_ranges', 'id');
+        $this->execute('DROP TRIGGER IF EXISTS sorm_redirect_ranges_after_ins_tr');
+        $this->execute('DROP TRIGGER IF EXISTS sorm_redirect_ranges_after_upd_tr');
+        $this->execute('DROP TRIGGER IF EXISTS sorm_redirect_ranges_before_upd_tr');
+    }
+}
