7ce4b1a799b0dd4df5b1dd76b139583372834f2a
Андреев Дмитрий <adima@mcn.ru>
2024-08-12 15:16:11 +0300

    STAT-736-1

--- dao/account/UpdateBalanceHelper.php
+++ dao/account/UpdateBalanceHelper.php
@@ -11,18 +11,23 @@ class UpdateBalanceHelper
 
     public static function mergePaymentIntoBills(&$bills, $pays)
     {
+//        $isTest = $_GET['t'] ?? false;
         $cnt = 0;
         $billSum = $paySum = 0.0;
         $isNextBill = $isNextPay = true;
         $billIdx = null;
         $bill = $pay = null;
+//        $fnNf = fn(float $n) => number_format($n, 2, '.', '');
+//        $fnP = fn(bool $n) => $n ? '+' : '-';
+
+//        echo "<table bolder=1 cellpadding='4' style='border: 1px solid red; border-collapse: collapse;'>";
         do {
             if ($isNextBill) {
                 $bill = current($bills);
                 if ($bill) {
                     $billIdx = key($bills);
                     next($bills);
-                    $billSum = round($bill['sum'], 2);
+                    $billSum = (float)$bill['sum'];
 
                 }
             }
@@ -31,34 +36,61 @@ class UpdateBalanceHelper
                 $pay = current($pays);
                 if ($pay) {
                     next($pays);
-                    $paySum = round($pay['sum'], 2);
-                }
-                if (!$pay) {
+                    $paySum = (float)$pay['sum'];
+                } else {
                     break;
                 }
             }
 
+//            echo "<tr>";
+//            echo "<td>" . ($bill ? $bill['bill_no'] . '</td><td>'.$fnNf($bill['sum']).'</td><td>'.$fnNf($billSum) : '</td><td></td><td>n/b').'</td>';
+//            echo '<td>' . ($pay ? $pay['id'] . '</td><td>'.($pay['sum']).'</td><td>' . $fnNf($paySum) : '</td><td></td><td>n/b') . '</td>';
+//            echo '<td>';
+
             if (!$bill && $pay) { // платежи кидаем на последний счет
-                $bills[$billIdx]['p'][] = $pay + ['sum_t' => round($paySum,2)];
+
+                $alreadPayIdx = null;
+                array_filter($bills[$billIdx]['p'], function($p, $k) use (&$alreadPayIdx, $pay) {$alreadPayIdx = $k; return $p['id'] == $pay['id'];}, ARRAY_FILTER_USE_BOTH);
+
+                if (is_null($alreadPayIdx)) {
+                    $bills[$billIdx]['p'][] = $pay + ['sum_t' => round($paySum, 2)];
+                } else {
+                    $bills[$billIdx]['p'][$alreadPayIdx]['sum_t'] += $paySum;
+                }
+
                 $isNextPay = true;
-            }elseif ($billSum == $paySum) {
+//                echo '1';
+            }elseif (abs(abs($billSum) - abs($paySum)) < 0.01) {
                 $bills[$billIdx]['new_is_payed'] = 1;
                 $bills[$billIdx]['p'][] = $pay + ['sum_t' => round($paySum,2)];
                 $isNextBill = $isNextPay = true;
+//                echo '2';
             } elseif (abs($billSum) > abs($paySum)) {
                 $bills[$billIdx]['new_is_payed'] = 2;
                 $bills[$billIdx]['p'][] = $pay + ['sum_t' => round($paySum,2)];
                 $billSum -= $paySum;
                 $isNextBill = false;
                 $isNextPay = true;
+//                echo '3';
             } elseif (abs($billSum) < abs($paySum)) {
                 $bills[$billIdx]['new_is_payed'] = 1;
                 $bills[$billIdx]['p'][] = $pay + ['sum_t' => round($billSum, 2)];
                 $paySum -= $billSum;
                 $isNextBill = true;
                 $isNextPay = false;
+//                echo '4';
+            } else {
+//                echo '5';
             }
+
+//            echo '</td><td>' . $fnP($isNextBill) . '</td><td>'.$fnP($isNextPay).'</td>';
+//            echo "</tr>";
         } while ($cnt++ < 1000);
+
+//        echo "</pre>";
+//        if ($isTest) {
+//            exit();
+//        }
     }
 
     public static function paymentOrders_extractFromBills($bills)
