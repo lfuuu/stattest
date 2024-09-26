d44a167395088d233fbb22484062f6f05c2a6572
Алексей Ильин <ilyin@mcn.ru>
2024-09-26 07:04:22 +0000

    Remove mock for test purposes

--- models/filter/PayReportFilter.php
+++ models/filter/PayReportFilter.php
@@ -225,8 +225,7 @@ class PayReportFilter extends Payment
                 $query->andWhere(['type' => $types]);
             }
         }
-        echo $query->createCommand()->getRawSql();
-        exit;
+
         $this->uuid !== '' && $query->andWhere([$paymentAtolTableName . '.uuid' => $this->uuid]);
         $this->uuid_status !== '' && $query->andWhere([$paymentAtolTableName . '.uuid_status' => $this->uuid_status]);
         if($this->uuid_log !== '') {
