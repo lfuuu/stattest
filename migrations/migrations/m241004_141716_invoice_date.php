67f9b6e8c3d54021a9e03f1a517a278c87c7db18
Алексей Ильин <ilyin@mcn.ru>
2024-10-07 11:48:06 +0000

    Refactor migrations

--- migrations/migrations/m241004_141716_invoice_date.php
+++ migrations/migrations/m241004_141716_invoice_date.php
@@ -12,6 +12,7 @@ class m241004_141716_invoice_date extends \app\classes\Migration
     {
         $this->addColumn('invoice', 'invoice_date', 'DATE NULL');
         $this->addColumn('newbills', 'invoice_date', 'DATE NULL');
+        $this->update('user_rights', ['values' => 'read,edit,delete,admin,del_docs,edit_ext,invoice_date', 'values_desc' => 'просмотр,изменение,удаление,изменение счета в любое время,Удаление отсканированных актов,Редактирование номера внешнего счета,Изменение даты счёт-фактуры'], ['resource' => 'newaccounts_bills']);
     }
 
     /**
@@ -21,5 +22,6 @@ class m241004_141716_invoice_date extends \app\classes\Migration
     {
         $this->dropColumn('invoice', 'invoice_date');
         $this->dropColumn('newbills', 'invoice_date');
+        $this->update('user_rights', ['values' => 'read,edit,delete,admin,del_docs,edit_ext', 'values_desc' => 'просмотр,изменение,удаление,изменение счета в любое время,Удаление отсканированных актов,Редактирование номера внешнего счета'], ['resource' => 'newaccounts_bills']);
     }
 }
