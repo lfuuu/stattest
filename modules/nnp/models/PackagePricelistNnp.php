473fc8dd46add15e821a76df0bed61c3e078cefe
Алексей Ильин <ilyin@mcn.ru>
2024-10-09 10:01:07 +0000

    Add histrory for nnp package pricelist

--- modules/nnp/models/PackagePricelistNnp.php
+++ modules/nnp/models/PackagePricelistNnp.php
@@ -14,4 +14,16 @@ class PackagePricelistNnp extends PackagePricelist
             [['tariff_id', 'nnp_pricelist_id', 'minute'], 'integer'],
         ];
     }
+
+    public static function prepareHistoryValue($field, $value)
+    {
+        switch ($field) {
+            case 'nnp_pricelist_id':
+                if ($pricelist = Pricelist::findOne($value)) {
+                    return $pricelist->name;
+                }
+                break;
+        }
+        return parent::prepareHistoryValue($field, $value);
+    }
 }
