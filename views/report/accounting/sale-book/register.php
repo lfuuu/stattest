b184970d90d0f3f8486eb655f5ffd47e4bdec3f9
Андреев Дмитрий <adima@mcn.ru>
2025-07-24 17:32:25 +0300

    fix registry report

--- views/report/accounting/sale-book/register.php
+++ views/report/accounting/sale-book/register.php
@@ -8,38 +8,39 @@
        style="border-collapse: collapse; font: normal 8pt sans-serif; padding: 2px 2px 2px 2px;">
 
     <thead>
-        <tr>
-            <td rowspan="2" class="s" align="center">Код&#160;операции</td>
-            <td colspan="1" class="s" align="center">В&#160;том&#160;числе </br> </br> </br> </br> </br></td>
-            <td rowspan="2" class="s" align="center">
-                Общая стоимость</br>
-                реализованных</br>
-                (переданных)&#160;товаров</br>
-                (работ, услуг) по   </br>
-                видам</br>
-                освобождаемых от</br>
-                налогооблажения&#160;</br>операций,</br>
-                отраженных в </br> налоговой </br>  декларации по налогу </br>  на добавленную </br>  стоимость, руб. </td>
-            <td rowspan="2" class="s" align="center">
-                Наименование (ФИО)
-                контрагента (покупателя)
-            </td>
-            <td rowspan="2" class="s" align="center">ИНН</td>
-            <td rowspan="2" class="s" align="center">КПП</td>
-            <td colspan="4" class="s" align="center">Документы,&#160;подтверждающие&#160;право&#160;налогплательщика&#160;на&#160;налоговые&#160;льготы</td>
-        </tr>
-        <td class="s" align="center">вид&#160;операции,&#160;по&#160;которой</br> применена налоговая льгота</td>
-        <td class="s" align="center">Вид&#160;документа</td>
-        <td class="s" align="center">№</td>
-        <td class="s" align="center">Дата</td>
-        <td class="s" align="center">
-            Общая&#160;стоимость&#160;реализованных </br>
-            (переданных)&#160;товаров&#160;(работ,&#160;услуг) </br>
-            по&#160;контрагенту </br>
-            или&#160;в&#160;случае&#160;наличия&#160;типового </br>
-            договора&#160;по&#160;нескольким </br>
-            контрагентам,&#160;руб.
+    <tr>
+        <td rowspan="2" class="s" align="center">Код&#160;операции</td>
+        <td colspan="1" class="s" align="center">В&#160;том&#160;числе </br> </br> </br> </br> </br></td>
+        <td rowspan="2" class="s" align="center">
+            Общая стоимость</br>
+            реализованных</br>
+            (переданных)&#160;товаров</br>
+            (работ, услуг) по   </br>
+            видам</br>
+            освобождаемых от</br>
+            налогооблажения&#160;</br>операций,</br>
+            отраженных в </br> налоговой </br>  декларации по налогу </br>  на добавленную </br>  стоимость, руб.
         </td>
+        <td rowspan="2" class="s" align="center">
+            Наименование (ФИО)
+            контрагента (покупателя)
+        </td>
+        <td rowspan="2" class="s" align="center">ИНН</td>
+        <td rowspan="2" class="s" align="center">КПП</td>
+        <td colspan="4" class="s" align="center">Документы,&#160;подтверждающие&#160;право&#160;налогплательщика&#160;на&#160;налоговые&#160;льготы</td>
+    </tr>
+    <td class="s" align="center">вид&#160;операции,&#160;по&#160;которой</br> применена налоговая льгота</td>
+    <td class="s" align="center">Вид&#160;документа</td>
+    <td class="s" align="center">№</td>
+    <td class="s" align="center">Дата</td>
+    <td class="s" align="center">
+        Общая&#160;стоимость&#160;реализованных </br>
+        (переданных)&#160;товаров&#160;(работ,&#160;услуг) </br>
+        по&#160;контрагенту </br>
+        или&#160;в&#160;случае&#160;наличия&#160;типового </br>
+        договора&#160;по&#160;нескольким </br>
+        контрагентам,&#160;руб.
+    </td>
     </thead>
     <tbody>
     <?php
@@ -77,21 +78,36 @@
                 $sum_tax = $invoice->sum_tax;
 
                 $linesSum16 = 0;
-                foreach($invoice->lines as $line) {
-                    if (!($line->date_to <= $filter->dateTo->format(DateTimeZoneHelper::DATE_FORMAT) && $line->date_from >= $filter->dateFrom->format(DateTimeZoneHelper::DATE_FORMAT))){
+                foreach ($invoice->lines as $line) {
+
+                    echo PHP_EOL . $line->item;
+                    if (!(
+                        $line->date_to <= $filter->dateTo->format(DateTimeZoneHelper::DATE_FORMAT)
+                        && $line->date_from >= $filter->dateFrom->format(DateTimeZoneHelper::DATE_FORMAT)
+                    )
+                    ) {
                         continue;
                     }
+
                     if ($line->line->id_service) {
                         if (
-                            ($filter->is_register_vp && in_array($line->line->accountTariff->service_type_id, [ServiceType::ID_VPBX, ServiceType::ID_VOIP]))
-                            || ($filter->is_register && $line->line->accountTariff->service_type_id = ServiceType::ID_VPBX)
+                            ($filter->is_register && $line->line->accountTariff->service_type_id == ServiceType::ID_VPBX)
+                            || ($filter->is_register_vp && in_array($line->line->accountTariff->service_type_id, [ServiceType::ID_VPBX, ServiceType::ID_VOIP]))
                         ) {
                             // pass
                         } else {
                             continue;
                         }
+                    } else {
+                        if ($filter->is_register && strpos($line->item, 'ВАТС') !== false) {
+                            // pass
+                        } elseif ($filter->is_register_vp && (strpos($line->item, 'ВАТС') !== false || strpos($line->item, 'Телефон') !== false)) {
+                            // pass
+                        } else {
+                            continue;
+                        }
                     }
-                    $linesSum16 += abs($line['sum_tax']) > 0  ? 0 : $line['sum'];
+                    $linesSum16 += abs($line['sum_tax']) > 0 ? 0 : $line['sum'];
                 }
 
                 if (abs($linesSum16) < 0.05) {
@@ -106,7 +122,7 @@
             $total['sumAll'] += $sum;
             $total['sumCol16'] += $linesSum16 ?: 0;
 
-            $contract =  $invoice->bill->clientAccount->contract;
+            $contract = $invoice->bill->clientAccount->contract;
             $contractDate = isset($contract->document->contract_date) ? $contract->document->contract_date : $contract->offer_date;
             $contractDate = $contractDate ?: '2021-09-01';
 
@@ -125,7 +141,7 @@
                 <td colspan="1">Лицензионное соглашение</td>
                 <td colspan="1"><?= $contract->number ?></td>
                 <td colspan="1"><?= date('d.m.Y', strtotime($contractDate)) ?></td>
-                <td rowspan="2" align="right"><?=  $printSum($linesSum16) ?></td>
+                <td rowspan="2" align="right"><?= $printSum($linesSum16) ?></td>
             </tr>
             <td>Акт</td>
             <td><?= $invoice->number ?></td>
